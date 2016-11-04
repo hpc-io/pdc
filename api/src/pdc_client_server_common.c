67eff51520bd062816048b703cd574f4b69f9a34
Houjun Tang <houj.tang@gmail.com>
2016-11-04 16:03:07 -0700

    Server uses multi threads for object id generation, currently using Mercury multi-thread functions. More testing and performance evaluation to be done.

--- api/src/pdc_client_server_common.c
+++ api/src/pdc_client_server_common.c
@@ -1,6 +1,14 @@
 #include "mercury.h"
+#include "mercury_thread_pool.h"
+#include "mercury_atomic.h"
+#include "mercury_thread_mutex.h"
+
 #include "pdc_interface.h"
 #include "pdc_client_server_common.h"
+
+// Thread
+hg_thread_pool_t *hg_test_thread_pool_g;
+
 
 uint64_t pdc_id_seq_g = 1000;
 // actual value for each server is set by PDC_Server_init()
@@ -8,13 +16,46 @@ uint64_t pdc_id_seq_g = 1000;
 uint64_t PDCS_gen_obj_id()
 {
     FUNC_ENTER(NULL);
-
     uint64_t ret_value;
     ret_value = pdc_id_seq_g++;
-
 done:
     FUNC_LEAVE(ret_value);
 }
+
+// Macros for multi-thread callback, grabbed from Mercury/Testing/mercury_rpc_cb.c
+#define HG_TEST_RPC_CB(func_name, handle) \
+    static hg_return_t \
+    func_name ## _thread_cb(hg_handle_t handle)
+
+/* Assuming func_name_cb is defined, calling HG_TEST_THREAD_CB(func_name)
+ * will define func_name_thread and func_name_thread_cb that can be used
+ * to execute RPC callback from a thread
+ */
+#define HG_TEST_THREAD_CB(func_name) \
+        static HG_THREAD_RETURN_TYPE \
+        func_name ## _thread \
+        (void *arg) \
+        { \
+            hg_handle_t handle = (hg_handle_t) arg; \
+            hg_thread_ret_t thread_ret = (hg_thread_ret_t) 0; \
+            \
+            func_name ## _thread_cb(handle); \
+            \
+            return thread_ret; \
+        } \
+        hg_return_t \
+        func_name ## _cb(hg_handle_t handle) \
+        { \
+            struct hg_thread_work *work = hg_core_get_thread_work(handle); \
+            hg_return_t ret = HG_SUCCESS; \
+            \
+            work->func = func_name ## _thread; \
+            work->args = handle; \
+            hg_thread_pool_post(hg_test_thread_pool_g, work); \
+            \
+            return ret; \
+        }
+
 
 /*
  * The routine that sets up the routines that actually do the work.
@@ -24,8 +65,9 @@ done:
  *
  * This callback/handler triggered upon receipt of rpc request
  */
-static hg_return_t
-gen_obj_id_cb(hg_handle_t handle)
+/* static hg_return_t */
+/* gen_obj_id_cb(hg_handle_t handle) */
+HG_TEST_RPC_CB(gen_obj_id, handle)
 {
     FUNC_ENTER(NULL);
 
@@ -53,6 +95,8 @@ done:
     FUNC_LEAVE(ret_value);
 }
 
+HG_TEST_THREAD_CB(gen_obj_id)
+
 hg_id_t
 gen_obj_id_register(hg_class_t *hg_class)
 {
@@ -64,3 +108,22 @@ gen_obj_id_register(hg_class_t *hg_class)
 done:
     FUNC_LEAVE(ret_value);
 }
+
+/* static hg_thread_ret_t */ 
+/* gen_obj_id_cb_thread (void *arg) */ 
+/* { */ 
+/*     hg_handle_t handle = (hg_handle_t) arg; */ 
+/*     hg_thread_ret_t thread_ret = (hg_thread_ret_t) 0; */ 
+/*     gen_obj_id_cb_thread_cb(handle); */ 
+/*     return thread_ret; */ 
+/* } */ 
+
+/* hg_return_t */ 
+/* gen_obj_id_cb_cb(hg_handle_t handle) */ 
+/* { */ 
+/*     hg_return_t ret = HG_SUCCESS; */ 
+/*     hg_thread_pool_post(hg_test_thread_pool_g, gen_obj_id_cb_thread, handle); */ 
+/*     return ret; */ 
+/* } */
+
+
