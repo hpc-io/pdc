1853df0328bda3e014f921c5cc6b43eb4a145746
“Kimmy <kmu@hdfgroup.org>
2016-12-06 09:56:19 -0800

    add more fields for pdc server work

--- api/src/pdc_prop_pkg.h
+++ api/src/pdc_prop_pkg.h
@@ -2,6 +2,7 @@
 #define _pdc_prop_pkg_H
 
 #include "pdc_life.h"
+#include "pdc_private.h"
 
 typedef enum {
     PDC_CONT_CREATE = 0,
@@ -17,7 +18,12 @@ typedef struct PDC_obj_prop_t {
     int             ndim;
     uint64_t        *dims;
     PDC_var_type_t  type;
+    uint32_t        user_id;
+    char*           app_name;
+    uint32_t        time_step;
+    char*           tags;              //placeholder, may change in the future
     void            *buf;
+    pdcid_t         *region;
 }PDC_obj_prop_t;
 
 #endif
