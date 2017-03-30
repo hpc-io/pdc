859932c6b9b7070e4d36ba0111a855797b8855c2
Jerome Soumagne <jsoumagne@hdfgroup.org>
2017-03-30 01:07:33 -0500

    Fix and cleanup CMake build framework
    Add option to generate doxygen documentation
    
    Fix formatting of doc in pdc_cont.h
    
    Pass -lm to server lib dependencies
    
    Add doxygen files to generate doc

--- api/src/pdc_cont.h
+++ api/src/pdc_cont.h
@@ -8,79 +8,113 @@
 
 typedef PDC_id_info_t cont_handle;
 
-/* PDC container initialization
- * \param pc [IN] Pointer to PDC_CLASS_t struct
+/**
+ * PDC container initialization
+ *
+ * \param pc [IN]               Pointer to PDC_CLASS_t struct
+ *
  * \return Non-negative on success/Negative on failure
  */
 perr_t PDCcont_init(PDC_CLASS_t *pc);
 
-/* Create a container 
- * \param pdc_id [IN] Id of the PDC
- * \param cont_name [IN] Name of the container
- * \param cont_create_prop [IN] Id of container property,
- * returned by PDCprop_create(PDC_CONT_CREATE)
+/**
+ * Create a container
+ *
+ * \param pdc_id [IN]           Id of the PDC
+ * \param cont_name [IN]        Name of the container
+ * \param cont_create_prop [IN] Id of container property
+ *                              returned by PDCprop_create(PDC_CONT_CREATE)
+ *
  * \return Container id on success/Negative on failure
  */
 pdcid_t PDCcont_create(pdcid_t pdc_id, const char *cont_name, pdcid_t cont_create_prop);
 
-/* Open a container 
- * \param pdc_id [IN] Id of the PDC
- * \param cont_name [IN] Name of the container 
+/**
+ * Open a container
+ *
+ * \param pdc_id [IN]           Id of the PDC
+ * \param cont_name [IN]        Name of the container
+ *
  * \return Container id on success/Negative on failure
  */
 pdcid_t PDCcont_open(pdcid_t pdc_id, const char *cont_name);
 
-/* Iterate over containers within a PDC
- * \param pdc_id [IN] Id of the PDC
+/**
+ * Iterate over containers within a PDC
+ *
+ * \param pdc_id [IN]           Id of the PDC
+ *
  * \return Pointer to cont_handle struct/NULL on failure
  */
 cont_handle *PDCcont_iter_start(pdcid_t pdc_id);
 
-/* Check if container handle is pointing to NULL 
- * \param chandle [IN] Pointer to cont_handle struct, 
- * returned by PDCcont_iter_start(pdcid_t pdc_id)
+/**
+ * Check if container handle is pointing to NULL
+ *
+ * \param chandle [IN]          Pointer to cont_handle struct,
+ *                              returned by PDCcont_iter_start(pdcid_t pdc_id)
+ *
  * \return 1 on success/0 on failure
  */
 pbool_t PDCcont_iter_null(cont_handle *chandle);
 
-/* Move to the next container within a PDC
- * \param chandle [IN] Pointer to cont_handle struct, returned by PDCcont_iter_start(pdcid_t pdc_id)
+/**
+ * Move to the next container within a PDC
+ * \param chandle [IN]          Pointer to cont_handle struct, returned by
+ *                              PDCcont_iter_start(pdcid_t pdc_id)
+ *
  * \return Pointer to cont_handle struct/NULL on failure
  */
 cont_handle *PDCcont_iter_next(cont_handle *chandle);
 
-/* Retrieve container information
- * \param chandle [IN] A cont_handle struct, returned by PDCcont_iter_start(pdcid_t pdc_id)
+/**
+ * Retrieve container information
+ *
+ * \param chandle [IN]          A cont_handle struct, returned by
+ *                              PDCcont_iter_start(pdcid_t pdc_id)
+ *
  * \return Pointer to a PDC_cont_info_t struct/NULL on failure
  */
 PDC_cont_info_t * PDCcont_iter_get_info(cont_handle *chandle);
 
-/* Persist a transient container
- * \param cont_id [IN] Id of the container, 
- * returned by PDCcont_open(pdcid_t pdc_id, const char *cont_name)
- * \param pdc_id [IN] Id of the PDC
+/**
+ * Persist a transient container
+ *
+ * \param cont_id [IN]          Id of the container, returned by
+ *                              PDCcont_open(pdcid_t pdc_id, const char *cont_name)
+ * \param pdc_id [IN]           Id of the PDC
+ *
  * \return Non-negative on success/Negative on failure
  */
 perr_t PDCcont_persist(pdcid_t cont_id, pdcid_t pdc_id);
 
-/* Set container lifetime 
- * \param cont_create_prop [IN] Id of container property, 
- * returned by PDCprop_create(PDC_CONT_CREATE)
- * \param cont_lifetime [IN] container lifetime (enum type), PDC_PERSIST or PDC_TRANSIENT
- * \param pdc_id [IN] Id of the PDC
+/**
+ * Set container lifetime
+ *
+ * \param cont_create_prop [IN] Id of container property, returned by
+ *                              PDCprop_create(PDC_CONT_CREATE)
+ * \param cont_lifetime [IN]    container lifetime (enum type), PDC_PERSIST or
+ *                              PDC_TRANSIENT
+ * \param pdc_id [IN]           Id of the PDC
+ *
  * \return Non-negative on success/Negative on failure
  */
 perr_t PDCprop_set_cont_lifetime(pdcid_t cont_create_prop, PDC_lifetime cont_lifetime, pdcid_t pdc_id);
 
-/* Close a container 
- * \param cont_id [IN] Container id, 
- * returned by PDCcont_open(pdcid_t pdc_id, const char *cont_name)
- * \param pdc_id [IN] Id of the PDC
+/**
+ * Close a container
+ *
+ * \param cont_id [IN]          Container id, returned by
+ *                              PDCcont_open(pdcid_t pdc_id, const char *cont_name)
+ * \param pdc_id [IN]           Id of the PDC
+ *
  * \return Non-negative on success/Negative on failure
  */
 perr_t PDCcont_close(pdcid_t cont_id, pdcid_t pdc);
 
-/* PDC container finalize
+/**
+ * PDC container finalize
+ *
  * \return Non-negative on success/Negative on failure
  */
 perr_t PDCcont_end();
