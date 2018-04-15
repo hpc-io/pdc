94f059b778200601e19ef0f30b5f954932288bd1
Jingqing Mu <kmu@hdfgroup.org>
2018-04-15 07:14:39 -0700

    less message print

--- src/server/pdc_server.c
+++ src/server/pdc_server.c
@@ -2717,8 +2717,8 @@ perr_t PDC_Server_init(int port, hg_class_t **hg_class, hg_context_t **hg_contex
     }
     MPI_Bcast(&credential, 1, MPI_UINT32_T, 0, MPI_COMM_WORLD);
     
-    printf("# Credential is %u\n", credential);
-    fflush(stdout);
+    // printf("# Credential is %u\n", credential);
+    // fflush(stdout);
 
     rc = drc_access(credential, 0, &credential_info);
     if (rc != DRC_SUCCESS) { /* failed to access credential */
@@ -2729,8 +2729,10 @@ perr_t PDC_Server_init(int port, hg_class_t **hg_class, hg_context_t **hg_contex
     }
     cookie = drc_get_first_cookie(credential_info);
 
-    printf("# Cookie is %u\n", cookie);
-    fflush(stdout);
+    if(pdc_server_rank_g == 0) {
+        printf("# Cookie is %u\n", cookie);
+        fflush(stdout);
+    }
     sprintf(pdc_auth_key, "%u", cookie);
     init_info.na_init_info.auth_key = strdup(pdc_auth_key);
 #endif
