93df8b7385f09c84f33fea6ee2c8df0b9f42b168
Houjun Tang <htang4@lbl.gov>
2019-01-25 10:30:58 -0800

    Move PDC_var_type_t to public header

--- src/api/pdc_public.h
+++ src/api/pdc_public.h
@@ -35,4 +35,20 @@ typedef bool                        pbool_t;
 typedef int                         PDC_int_t;
 typedef float                       PDC_float_t;
 typedef double                      PDC_double_t;
+
+typedef enum {
+    PDC_UNKNOWN      = -1, /* error                                      */
+    PDC_INT          = 0,  /* integer types                              */
+    PDC_FLOAT        = 1,  /* floating-point types                       */
+    PDC_DOUBLE       = 2,  /* double types                               */
+    PDC_STRING       = 3,  /* character string types                     */
+    PDC_COMPOUND     = 4,  /* compound types                             */
+    PDC_ENUM         = 5,  /* enumeration types                          */
+    PDC_ARRAY        = 6,  /* Array types                                */
+    PDC_INT64        = 7,  /* 64-bit integer types                       */
+ 
+    NCLASSES         = 8   /* this must be last                          */
+} PDC_var_type_t;
+
+
 #endif
