8626cc4e12d345ca3bee68fa43767a18d5956faa
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-01-25 14:03:55 -0600

    Update mpi_test.sh

--- src/tests/mpi_test.sh
+++ src/tests/mpi_test.sh
@@ -12,6 +12,7 @@ n_client="$4"
 shift
 # copy the remaining test input arguments (if any)
 test_args="${5} ${6}"
+echo "Input arguments are the followings"
 echo $test_args
 if [ -x $test_exe ]; then echo "testing: $test_exe"; else echo "test: $test_exe not found or not and executable" && exit -2; fi
 rm -rf pdc_tmp
