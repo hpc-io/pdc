51bfc935624db1053593b967609e947e0a03d063
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-01-25 13:47:48 -0600

    mpi test

--- src/tests/mpi_test.sh
+++ src/tests/mpi_test.sh
@@ -6,19 +6,22 @@
 if [ $# -lt 1 ]; then echo "missing test argument" && exit -1 ; fi
 # check the test to be run:
 test_exe="$1"
+mpi_cmd="$2"
+n_servers="$3"
+n_client="$4"
 shift
 # copy the remaining test input arguments (if any)
 test_args="$*"
 if [ -x $test_exe ]; then echo "testing: $test_exe"; else echo "test: $test_exe not found or not and executable" && exit -2; fi
 rm -rf pdc_tmp
 # START the server (in the background)
-mpiexec -n 4 ./pdc_server.exe &
+$mpi_cmd -n $n_servers ./pdc_server.exe &
 # WAIT a bit, for 1 second
 sleep 1
 # RUN the actual test
-mpiexec -n 4 $test_exe $*
+$mpi_cmd -n $n_client $test_exe $*
 # Need to test the return value
 ret="$?"
 # and shutdown the SERVER before exiting
-./close_server
+$mpi_cmd -n 1 ./close_server
 exit $ret
