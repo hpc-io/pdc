f5856cfb3c0720c2d0e0dd1490a041aeefd39afe
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-01-25 18:09:10 -0600

    Update write_obj.c

--- src/tests/write_obj.c
+++ src/tests/write_obj.c
@@ -58,7 +58,7 @@ int main(int argc, char **argv)
         return ret_value;
     }
 
-    sprintf(obj_name, "%s_%d", argv[1], rank);
+    sprintf(obj_name, "%s", argv[1]);
     size_MB = atoi(argv[2]);
 
     if (rank == 0) {
