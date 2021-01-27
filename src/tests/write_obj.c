90fd1fb6971da4b50b72c62e724b42cad65a7041
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-01-27 14:05:33 -0600

    Update write_obj.c

--- src/tests/write_obj.c
+++ src/tests/write_obj.c
@@ -105,7 +105,7 @@ int main(int argc, char **argv)
     // Create a object
     //global_obj = PDCobj_create_mpi(cont, obj_name, obj_prop, 0, comm);
     sprintf(obj_name, "o1_%d", rank);
-    obj1 = PDCobj_create(cont, obj_name, obj_prop);
+    global_obj = PDCobj_create(cont, obj_name, obj_prop);
 
     if (global_obj <= 0) {
         printf("Error creating an object [%s], exit...\n", obj_name);
