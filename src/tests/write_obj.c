55a8c0863f65217ab2ad27aecb0fa7cf97037b96
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-01-29 20:02:30 -0600

    Update write_obj.c

--- src/tests/write_obj.c
+++ src/tests/write_obj.c
@@ -126,8 +126,8 @@ int main(int argc, char **argv)
     my_data_size = size_B / size;
     printf("my_data_size at rank %d is %llu\n", rank, (long long unsigned)my_data_size);
 
-    obj_data = (char*)malloc(my_data_size*type_size);
-    mydata = (char*)malloc(my_data_size*type_size);
+    obj_data = (char*)malloc(my_data_size*type_size*10);
+    mydata = (char*)malloc(my_data_size*type_size*10);
 
 
     PDCprop_set_obj_type(obj_prop, var_type);
