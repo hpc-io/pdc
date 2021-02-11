ca86d297a91c56533a4e1e7164f83a6d0012b556
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-02-11 15:41:35 -0600

    Update pdc_client_connect.c

--- src/api/pdc_client_connect.c
+++ src/api/pdc_client_connect.c
@@ -6197,7 +6197,7 @@ PDCobj_put_data(const char *obj_name, void *data, uint64_t size, pdcid_t cont_id
                 pdc_client_mpi_rank_g, obj_name);
     }
 
-    ret = PDCreg_release_lock(obj_id, obj_region, PDC_WRITE);
+    ret = PDCreg_release_lock(obj_id, obj_region, PDC_BLOCK);
     if (ret != SUCCEED) {
         PGOTO_ERROR(0, "==PDC_CLIENT[%d]: Error with PDCreg_release_lock for obj [%s]",
                 pdc_client_mpi_rank_g, obj_name);
