39e71e328d9a7bc823e9df3ee3538baba2fd940a
Houjun Tang <houj.tang@gmail.com>
2021-03-15 16:20:23 -0700

    Update workflow

--- .github/workflows/dependencies-linux.sh
+++ .github/workflows/dependencies-linux.sh
@@ -10,7 +10,7 @@ git clone https://github.com/ofiwg/libfabric.git
 cd libfabric
 ./autogen.sh
 ./configure --disable-usnic --disable-mrail --disable-rstream --disable-perf --disable-efa --disable-psm2 --disable-psm --disable-verbs --disable-shm --disable-static --disable-silent-rules
-make -j2 && make install
+make -j2 && sudo make install
 make check
 cd ..
 
@@ -19,5 +19,5 @@ git clone --recursive https://github.com/mercury-hpc/mercury.git
 cd mercury
 mkdir build && cd build
 cmake ../  -DCMAKE_C_COMPILER=gcc -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=ON -DNA_USE_OFI=ON -DNA_USE_SM=OFF
-make -j2 && make install
+make -j2 && sudo make install
 ctest
