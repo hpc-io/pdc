0fea39b5570454b9c15b5936c3bb7474b7589a1f
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-04-16 21:42:13 -0500

    test

--- src/server/pdc_server_analysis.h
+++ src/server/pdc_server_analysis.h
@@ -32,30 +32,6 @@
 #define PDC_TIMING 1
 
 #if PDC_TIMING == 1
-typedef struct pdc_server_timing {
-    double PDCbuf_obj_map_rpc;
-    double PDCbuf_obj_unmap_rpc;
-    double PDCreg_obtain_lock_rpc;
-    double PDCreg_release_lock_rpc;
-} pdc_server_timing;
-
-typedef struct pdc_server_timestamp {
-    double *start;
-    double *end;
-    size_t timestamp_max_size;
-    size_t timestamp_size;
-} pdc_server_timestamp;
-
-int PDC_server_timing_init();
-int PDC_server_timing_report();
-int PDC_server_timestamp_register(pdc_server_timestamp *timestamp, double start, double end);
-
-pdc_server_timing *server_timings;
-pdc_server_timestamp *buf_obj_map_timestamps;
-pdc_server_timestamp *buf_obj_unmap_timestamps;
-pdc_server_timestamp *obtain_lock_timestamps;
-pdc_server_timestamp *release_lock_timestamps;
-double base_time;
 
 #endif
 
