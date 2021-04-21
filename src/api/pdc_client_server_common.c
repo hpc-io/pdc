9c9a603e2d2a01ce730a076d499411dbddab2764
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-04-21 17:30:23 -0500

    Update pdc_client_server_common.c

--- src/api/pdc_client_server_common.c
+++ src/api/pdc_client_server_common.c
@@ -79,6 +79,15 @@ int PDC_timing_init() {
     client_buf_obj_unmap_timestamps = client_buf_obj_map_timestamps + 1;
     client_obtain_lock_timestamps = client_buf_obj_map_timestamps + 2;
     client_release_lock_timestamps = client_buf_obj_map_timestamps + 3;
+
+    client_buf_obj_map_timestamps->timestamp_size = 0;
+    client_buf_obj_map_timestamps->timestamp_max_size = 0;
+    client_buf_obj_unmap_timestamps->timestamp_size = 0;
+    client_buf_obj_unmap_timestamps->timestamp_max_size = 0;
+    client_obtain_lock_timestamps->timestamp_size = 0;
+    client_obtain_lock_timestamps->timestamp_max_size = 0;
+    client_release_lock_timestamps->timestamp_size = 0;
+    client_release_lock_timestamps->timestamp_max_size = 0;
 }
 
 
