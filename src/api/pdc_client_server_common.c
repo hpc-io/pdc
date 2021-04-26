487ddd5738f436a56f995196dfd4444d342f7adc
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-04-26 17:37:00 -0500

    Update pdc_client_server_common.c

--- src/api/pdc_client_server_common.c
+++ src/api/pdc_client_server_common.c
@@ -178,7 +178,6 @@ int PDC_server_timing_report() {
         printf("rank = %d, maximum timing among all processes, PDCbuf_obj_map_rpc = %lf, PDCreg_obtain_lock_rpc = %lf, PDCreg_release_lock_rpc = %lf, PDCbuf_obj_unmap_rpc = %lf\n", rank, max_timings.PDCbuf_obj_map_rpc, max_timings.PDCreg_obtain_lock_rpc, max_timings.PDCreg_release_lock_rpc, max_timings.PDCbuf_obj_unmap_rpc);
     }
 
-    stream = fopen(filename,"w");
 
     sprintf(filename, "pdc_server_log_rank_%d.csv", rank);
 
