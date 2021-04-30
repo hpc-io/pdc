85f781e20b283e8d2bbbe95005c1a99e96b75086
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-04-30 01:10:17 -0500

    Add a few more tests

---
+++ src/tests/cont_del.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright Notice for 
+ * Proactive Data Containers (PDC) Software Library and Utilities
+ * -----------------------------------------------------------------------------
+
+ *** Copyright Notice ***
+ 
+ * Proactive Data Containers (PDC) Copyright (c) 2017, The Regents of the
+ * University of California, through Lawrence Berkeley National Laboratory,
+ * UChicago Argonne, LLC, operator of Argonne National Laboratory, and The HDF
+ * Group (subject to receipt of any required approvals from the U.S. Dept. of
+ * Energy).  All rights reserved.
+ 
+ * If you have questions about your rights to use or distribute this software,
+ * please contact Berkeley Lab's Innovation & Partnerships Office at  IPO@lbl.gov.
+ 
+ * NOTICE.  This Software was developed under funding from the U.S. Department of
+ * Energy and the U.S. Government consequently retains certain rights. As such, the
+ * U.S. Government has been granted for itself and others acting on its behalf a
+ * paid-up, nonexclusive, irrevocable, worldwide license in the Software to
+ * reproduce, distribute copies to the public, prepare derivative works, and
+ * perform publicly and display publicly, and to permit other to do so.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pdc.h"
+
+int main(int argc, char **argv) 
+{
+    pdcid_t pdc, create_prop, cont;
+    int rank = 0, size = 1;
+    perr_t ret;
+
+    int ret_value = 0;
+    char cont_name[128];
+
+#ifdef ENABLE_MPI
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+#endif
+    sprintf(cont_name, "c%d", rank);
+    // create a pdc
+    pdc = PDCinit("pdc");
+    printf("create a new pdc\n");
+
+    // create a container property
+    create_prop = PDCprop_create(PDC_CONT_CREATE, pdc);
+    if(create_prop > 0) {
+        printf("Create a container property\n");
+    } else {
+        printf("Fail to create container property @ line  %d!\n", __LINE__);
+        ret_value = 1;
+    }
+    // create a container
+    cont = PDCcont_create(cont_name, create_prop);
+    if(cont > 0) {
+        printf("Create a container c1\n");
+    } else {
+        printf("Fail to create container @ line  %d!\n", __LINE__);
+        ret_value = 1;
+    }
+    ret = PDCcont_del(cont);
+    if (ret != SUCCEED) {
+        printf("Fail to delete container @ line  %d!\n", __LINE__);
+        ret_value = 1;
+    }
+    // close a container
+    if(PDCcont_close(cont) < 0) {
+        printf("fail to close container c1\n");
+        ret_value = 1;
+    } else {
+        printf("successfully close container c1\n");
+    }
+
+    // close a container property
+    if(PDCprop_close(create_prop) < 0) {
+        printf("Fail to close property @ line %d\n", __LINE__);
+        ret_value = 1;
+    } else {
+        printf("successfully close container property\n");
+    }
+    // close pdc
+    if(PDCclose(pdc) < 0) {
+        printf("fail to close PDC\n");
+        ret_value = 1;
+    }
+#ifdef ENABLE_MPI
+    MPI_Finalize();
+#endif
+    return ret_value;
+
+}
