9c980c6432c199ec72c9018231fe31217185232a
Houjun Tang <htang4@lbl.gov>
2021-04-29 17:26:06 -0700

    Remove time-consuming tests of dependent libraries

--- .github/workflows/dependencies-linux.sh
+++ .github/workflows/dependencies-linux.sh
@@ -11,7 +11,7 @@ cd libfabric
 ./autogen.sh
 ./configure --disable-usnic --disable-mrail --disable-rstream --disable-perf --disable-efa --disable-psm2 --disable-psm --disable-verbs --disable-shm --disable-static --disable-silent-rules
 make -j2 && sudo make install
-make check
+# make check
 cd ..
 
 # Mercury
@@ -20,4 +20,4 @@ cd mercury
 mkdir build && cd build
 cmake ../  -DCMAKE_C_COMPILER=gcc -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=ON -DNA_USE_OFI=ON -DNA_USE_SM=OFF
 make -j2 && sudo make install
-ctest
+# ctest
