5a3c677ea993111798bce32168055d5a14783fc9
Houjun Tang <htang4@lbl.gov>
2021-06-08 13:35:34 -0700

    Switch libfabric version

--- .github/workflows/dependencies-linux.sh
+++ .github/workflows/dependencies-linux.sh
@@ -6,12 +6,14 @@ sudo apt-get update
 sudo apt-get install libopenmpi-dev
 
 # libfabric
-git clone https://github.com/ofiwg/libfabric.git
-cd libfabric
+# git clone https://github.com/ofiwg/libfabric.git
+wget https://github.com/ofiwg/libfabric/archive/refs/tags/v1.12.1.tar.gz
+tar xf v1.12.1.tar.gz
+cd libfabric-1.12.1
 ./autogen.sh
 ./configure --disable-usnic --disable-mrail --disable-rstream --disable-perf --disable-efa --disable-psm2 --disable-psm --disable-verbs --disable-shm --disable-static --disable-silent-rules
 make -j2 && sudo make install
-# make check
+make check
 cd ..
 
 # Mercury
@@ -20,4 +22,4 @@ cd mercury
 mkdir build && cd build
 cmake ../  -DCMAKE_C_COMPILER=gcc -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=ON -DNA_USE_OFI=ON -DNA_USE_SM=OFF
 make -j2 && sudo make install
-# ctest
+ctest
