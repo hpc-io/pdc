74a888a1e857392faa09b5c3b24424d59251a9ab
Houjun Tang <htang4@lbl.gov>
2021-06-14 17:58:51 -0700

    Add cori specific commands to mpi_test.sh script

--- src/tests/mpi_test.sh
+++ src/tests/mpi_test.sh
@@ -3,6 +3,11 @@
 # We assume too, that if the library build has enabled MPI, that LD_LIBRARY_PATH is
 # defined and points to the MPI libraries used by the linker (e.g. -L<path -lmpi)
 
+extra_cmd=""
+if [[ "$HOSTNAME" == "cori"* ]]; then
+    extra_cmd="--mem=25600 --cpu_bind=cores --gres=craynetwork:1 "
+fi
+
 if [ $# -lt 1 ]; then echo "missing test argument" && exit -1 ; fi
 # check the test to be run:
 test_exe="$1"
@@ -17,13 +22,13 @@ echo $test_args
 if [ -x $test_exe ]; then echo "testing: $test_exe"; else echo "test: $test_exe not found or not and executable" && exit -2; fi
 rm -rf pdc_tmp
 # START the server (in the background)
-echo "$mpi_cmd -n $n_servers ./pdc_server.exe &"
-$mpi_cmd -n $n_servers ./pdc_server.exe &
+echo "$mpi_cmd -n $n_servers $extra_cmd ./pdc_server.exe &"
+$mpi_cmd -n $n_servers $extra_cmd ./pdc_server.exe &
 # WAIT a bit, for 1 second
 sleep 1
 # RUN the actual test
-echo "$mpi_cmd -n $n_client $test_exe $test_args"
-$mpi_cmd -n $n_client $test_exe $test_args
+echo "$mpi_cmd -n $n_client $extra_cmd $test_exe $test_args"
+$mpi_cmd -n $n_client $extra_cmd $test_exe $test_args
 # Need to test the return value
 ret="$?"
 # and shutdown the SERVER before exiting
