b279224c2aabb4ed0011e074083df0b0ad8111d8
Houjun Tang <htang4@lbl.gov>
2021-06-18 11:54:05 -0700

    Update test script for CI on Cori

--- src/tests/run_multiple_test.sh
+++ src/tests/run_multiple_test.sh
@@ -3,6 +3,12 @@
 # We assume too, that if the library build has enabled MPI, that LD_LIBRARY_PATH is
 # defined and points to the MPI libraries used by the linker (e.g. -L<path -lmpi)
 
+# Cori CI needs srun even for serial tests
+run_cmd=""
+if [[ "$HOSTNAME" == "cori"* || "$HOSTNAME" == "nid"* ]]; then
+    run_cmd="srun -n 1 --mem=25600 --cpu_bind=cores --gres=craynetwork:1 "
+fi
+
 if [ $# -lt 1 ]; then echo "missing test argument" && exit -1 ; fi
 # check the test to be run:
 # test_exe="$1"
@@ -11,14 +17,14 @@ if [ $# -lt 1 ]; then echo "missing test argument" && exit -1 ; fi
 test_args="$*"
 # if [ -x $test_exe ]; then echo "testing: $test_exe"; else echo "test: $test_exe not found or not and executable" && exit -2; fi
 # START the server (in the background)
-./pdc_server.exe &
+$run_cmd ./pdc_server.exe &
 # WAIT a bit...
 sleep 1
 # RUN the actual test(s)
 for test_exe in $test_args
 do
     echo "testing: $test_exe"
-    $test_exe
+    $run_cmd $test_exe
 done
 # and shutdown the SERVER before exiting
-./close_server
+$run_cmd ./close_server
--- src/tests/run_test.sh
+++ src/tests/run_test.sh
@@ -3,6 +3,12 @@
 # We assume too, that if the library build has enabled MPI, that LD_LIBRARY_PATH is
 # defined and points to the MPI libraries used by the linker (e.g. -L<path -lmpi)
 
+# Cori CI needs srun even for serial tests
+run_cmd=""
+if [[ "$HOSTNAME" == "cori"* || "$HOSTNAME" == "nid"* ]]; then
+    run_cmd="srun -n 1 --mem=25600 --cpu_bind=cores --gres=craynetwork:1 "
+fi
+
 if [ $# -lt 1 ]; then echo "missing test argument" && exit -1 ; fi
 # check the test to be run:
 test_exe="$1"
@@ -12,14 +18,14 @@ shift
 if [ -x $test_exe ]; then echo "testing: $test_exe"; else echo "test: $test_exe not found or not and executable" && exit -2; fi
 rm -rf pdc_tmp
 # START the server (in the background)
-./pdc_server.exe &
+$run_cmd ./pdc_server.exe &
 # WAIT a bit...
 sleep 1
 # RUN the actual test
-echo "$test_exe $test_args"
-$test_exe $test_args
+echo "$run_cmd $test_exe $test_args"
+$run_cmd $test_exe $test_args
 # Need to test the return value
 ret="$?"
 # and shutdown the SERVER before exiting
-./close_server
+$run_cmd ./close_server
 exit $ret
