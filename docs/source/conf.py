1b162d5228595dad1f93ed10c7a1cd0aef02a8e1
Kenneth Casimiro <kennethreyescasimiro@gmail.com>
2021-07-01 15:45:00 -0700

    Revert "Fixed PDC logo not showing on readthedocs"
    This reverts commit 68c86c0d59a739ff4930e8ceb4a30132949894ac.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,8 +52,8 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['images']
-html_logo = "images/pdclogo.png"
+html_static_path = ['_static']
+html_logo = "_static/pdc_logo.png"
 # html_static_path = ['/Users/kenneth/Documents/Berkeley_Lab/pdc/docs/_static']
 # html_logo = "/Users/kenneth/Documents/Berkeley_Lab/pdc/docs/_static/pdc_logo.png"
 
