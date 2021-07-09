099276706c7f4f69cef7c24c5ed26ec6c5ffdb03
Kenneth Casimiro <kennethreyescasimiro@gmail.com>
2021-07-09 11:23:34 -0700

    Added Spack package recipe changes from alalazo

--- spack/package.py
+++ spack/package.py
@@ -1,3 +1,25 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install pdc
+#
+# You can edit this file again by typing:
+#
+#     spack edit pdc
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
 from spack import *
 
 
@@ -16,19 +38,19 @@ class Pdc(CMakePackage):
     conflicts('%clang')
     depends_on('libfabric@1.11.2')
     depends_on('mercury')
-    depends_on('cmake')
     depends_on('mpi')
 
     root_cmakelists_dir = 'src'
 
     def cmake_args(self):
-        args = []
-        args.append("-DMPI_C_COMPILER=%s" % self.spec['mpi'].mpicc)
-        args.append("-DBUILD_MPI_TESTING=ON")
-        args.append("-DBUILD_SHARED_LIBS=ON")
-        args.append("-DBUILD_TESTING=ON")
-        args.append("-DPDC_ENABLE_MPI=ON")
-        args.append("-DCMAKE_C_COMPILER=%s" % self.spec['mpi'].mpicc)
+        args = [
+            self.define('MPI_C_COMPILER', self.spec['mpi'].mpicc),
+            self.define('BUILD_MPI_TESTING', 'ON'),
+            self.define('BUILD_SHARED_LIBS', 'ON'),
+            self.define('BUILD_TESTING', 'ON'),
+            self.define('PDC_ENABLE_MPI', 'ON'),
+            self.define('CMAKE_C_COMPILER', self.spec['mpi'].mpicc)
+        ]
 
         if self.spec.satisfies('platform=cray'):
             args.append("-DRANKSTR_LINK_STATIC=ON")
