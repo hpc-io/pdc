927ca4069e1b45676c7c70965d616110fc0ce2d1
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-10-18 16:20:32 -0700

    Update pdc_client_server_common.c

--- src/api/pdc_client_server_common.c
+++ src/api/pdc_client_server_common.c
@@ -3662,6 +3662,7 @@ HG_TEST_RPC_CB(region_transform_release, handle)
     HG_Get_input(handle, &in);
     /* Get info from handle */
 
+
     hg_info = HG_Get_info(handle);
 
     if (in.access_type == PDC_READ)
@@ -4480,7 +4481,6 @@ PDC_Server_transfer_request_io(uint64_t obj_id, int obj_ndim, uint64_t *obj_dims
         // Check we can directly write the contiguous chunk to the file
         if (region_info->offset[1] == 0 && region_info->size[1] == obj_dims[1] &&
             region_info->offset[2] == 0 && region_info->size[2] == obj_dims[2]) {
-            printf("checkpoint 1\n");
             lseek(fd, region_info->offset[0] * region_info->size[1] * region_info->size[2] * unit, SEEK_SET);
             io_size = region_info->size[0] * region_info->size[1] * region_info->size[2] * unit;
             PDC_POSIX_IO(fd, buf, io_size, is_write);
