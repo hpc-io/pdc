ce7d433b284d9b9c176f06e9bec308e98551d661
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-10-19 13:05:00 -0700

    Update pdc_client_connect.c

--- src/api/pdc_client_connect.c
+++ src/api/pdc_client_connect.c
@@ -2393,7 +2393,7 @@ pack_region_buffer(char *buf, char **new_buf, size_t total_data_size, int local_
         if (access_type == PDC_WRITE) {
             ptr = *new_buf;
             for (i = 0; i < local_size[0]; ++i) {
-                memcpy(ptr, buf + (i * local_size[1] + local_offset[1]) * unit, local_size[1] * unit);
+                memcpy(ptr, buf + ( (local_offset[0] + i) * local_size[1] + local_offset[1]) * unit, local_size[1] * unit);
                 ptr += local_size[1] * unit;
             }
         }
@@ -2404,8 +2404,8 @@ pack_region_buffer(char *buf, char **new_buf, size_t total_data_size, int local_
             ptr = *new_buf;
             for (i = 0; i < local_size[0] * local_size[1]; ++i) {
                 memcpy(ptr,
-                       buf + ((i / (local_size[2] * local_size[1])) * local_size[2] * local_size[1] +
-                              (i % local_size[1]) * local_size[2] + local_offset[2]) *
+                       buf + ( (local_offset[0] +i / (local_size[2] * local_size[1])) * local_size[2] * local_size[1] +
+                              (local_offset[1] + i % local_size[1]) * local_size[2] + local_offset[2]) *
                                  unit,
                        local_size[2] * unit);
                 ptr += local_size[2] * unit;
@@ -2432,7 +2432,7 @@ release_region_buffer(char *buf, char *new_buf, int local_ndim, uint64_t *local_
         if (access_type == PDC_READ) {
             ptr = new_buf;
             for (i = 0; i < local_size[0]; ++i) {
-                memcpy(buf + (local_offset[0] * local_size[1] + local_offset[1]) * unit, ptr,
+                memcpy(buf + ( (local_offset[0] + i) * local_size[1] + local_offset[1]) * unit, ptr,
                        local_size[1] * unit);
                 ptr += local_size[1] * unit;
             }
@@ -2443,8 +2443,8 @@ release_region_buffer(char *buf, char *new_buf, int local_ndim, uint64_t *local_
         if (access_type == PDC_READ) {
             ptr = new_buf;
             for (i = 0; i < local_size[0] * local_size[1]; ++i) {
-                memcpy(buf + (local_offset[0] * local_size[1] * local_size[2] +
-                              local_offset[1] * local_size[2] + local_offset[2]) *
+                memcpy(buf + ( (local_offset[0] + i / (local_size[2] * local_size[1])) * local_size[2] * local_size[1] +
+                              (local_offset[1] + i % local_size[1]) * local_size[2] + local_offset[2]) *
                                  unit,
                        ptr, local_size[2] * unit);
                 ptr += local_size[2] * unit;
