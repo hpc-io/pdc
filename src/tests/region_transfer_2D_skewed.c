84eabaf22ed2e23c8fb4d60541d74d9bd88b2bb8
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-10-19 00:58:24 -0700

    test

--- src/tests/region_transfer_2D_skewed.c
+++ src/tests/region_transfer_2D_skewed.c
@@ -174,7 +174,7 @@ main(int argc, char **argv)
     offset_length[0] = BUF_LEN / 64;
     offset_length[1] = 16;
     reg_global       = PDCregion_create(2, offset, offset_length);
-
+    printf("checkpoint %d\n", __LINE__);
     transfer_request = PDCtransfer_request_create(data_read, PDC_READ, obj1, reg, reg_global);
 
     PDCtransfer_request(transfer_request);
