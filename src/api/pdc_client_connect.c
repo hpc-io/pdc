7174ba0fa12f50fe310cf72301e80181f16b8197
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-10-20 01:14:54 -0700

    Update pdc_client_connect.c

--- src/api/pdc_client_connect.c
+++ src/api/pdc_client_connect.c
@@ -2554,7 +2554,7 @@ PDC_Client_transfer_request(void *buf, pdcid_t obj_id, int obj_ndim, uint64_t *o
     work_todo_g = 1;
     PDC_Client_check_response(&send_context_g);
 
-    printf("PDC_Client_transfer_request() checkpoint @ line %d\n", __LINE__);
+    printf("PDC_Client_transfer_request() checkpoint, first value is %d @ line %d\n", (int)new_buf[sizeof(int)], __LINE__);
     release_region_buffer(buf, new_buf, obj_dims, local_ndim, local_offset, local_size, unit, access_type);
 
     if (transfer_args.ret != 1)
