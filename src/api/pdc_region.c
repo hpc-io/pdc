a5271ae45b7e479ae26ccffa5cf9c1a64fc70317
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-10-27 19:15:19 -0700

    Update pdc_region.c

--- src/api/pdc_region.c
+++ src/api/pdc_region.c
@@ -168,10 +168,10 @@ PDCregion_transfer_create(void *buf, pdc_access_t access_type, pdcid_t obj_id, p
     printf("PDCregion_transfer_create checkpoint @ line %d\n", __LINE__);
     reginfo1 = PDC_find_id(local_reg);
     reg1     = (struct pdc_region_info *)(reginfo1->obj_ptr);
-
+    printf("PDCregion_transfer_create checkpoint @ line %d\n", __LINE__);
     reginfo2 = PDC_find_id(remote_reg);
     reg2     = (struct pdc_region_info *)(reginfo2->obj_ptr);
-
+    printf("PDCregion_transfer_create checkpoint @ line %d\n", __LINE__);
     objinfo2 = PDC_find_id(obj_id);
     printf("PDCregion_transfer_create checkpoint @ line %d\n", __LINE__);
     if (objinfo2 == NULL)
