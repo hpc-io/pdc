454282368f4a8f05dcaeef231ba1c22de9ae1786
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-11-08 00:33:51 -0800

    Update pdc_client_server_common.c

--- src/api/pdc_client_server_common.c
+++ src/api/pdc_client_server_common.c
@@ -4514,15 +4514,16 @@ PDC_Server_transfer_request_io(uint64_t obj_id, int obj_ndim, const uint64_t *ob
     FUNC_ENTER(NULL);
 
     if (io_by_region_g) {
+        printf("checkpoint @line %d\n", __LINE__);
         if (is_write) {
             PDC_Server_data_write_out(obj_id, region_info, buf, unit);
         }
         else {
             PDC_Server_data_read_from(obj_id, region_info, buf, unit);
         }
+        printf("checkpoint @line %d\n", __LINE__);
         goto done;
     }
-
     if (obj_ndim != (int)region_info->ndim) {
         printf("Server I/O error: Obj dim does not match obj dim\n");
         goto done;
