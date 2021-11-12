ce11fa02116404dcb07bac8ab302fc8c060fd179
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-11-11 17:24:32 -0800

    Update pdc_server_data.c

--- src/server/pdc_server_data.c
+++ src/server/pdc_server_data.c
@@ -4994,7 +4994,8 @@ PDC_Server_data_read_from(uint64_t obj_id, struct pdc_region_info *region_info, 
                          pread(region->fd, buf + pos, overlap_count[0] * unit,
                                storage_region->offset + (overlap_start[0] - elt->start[0]) * unit)) !=
                     (ssize_t)(overlap_count[0] * unit)) {
-                    printf("==PDC_SERVER[%d]: pread failed to read enough bytes from offset %"PRIu64", expected = %" PRIu64
+                    printf("==PDC_SERVER[%d]: pread failed to read enough bytes from offset %" PRIu64
+                           ", expected = %" PRIu64
                            ", actual = %zu\n",
                            pdc_server_rank_g, storage_region->offset + (overlap_start[0] - elt->start[0]) * unit), overlap_count[0] * unit, (size_t)my_read_bytes);
                 }
