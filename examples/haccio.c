45af02a59e3a7e1aadabe2e04d515325e7d7e398
Chen Wang <wangvsa@gmail.com>
2022-01-20 15:44:29 -0600

    Include timing report feature (#51)
    * Include timing report feature
    
    * Correct clang format

--- examples/haccio.c
+++ examples/haccio.c
@@ -188,13 +188,17 @@ main(int argc, char **argv)
         free(buffers[i]);
     }
 
+#if PDC_TIMING == 1
+    PDC_timing_report("write");
+#endif
+
     PDCcont_close(cont_id);
     PDCprop_close(cont_prop);
     PDCclose(pdc_id);
 
     if (mpi_rank == 0) {
         double per_particle = sizeof(float) * 7 + sizeof(int64_t) + sizeof(int16_t);
-        double bandwidth    = per_particle * NUM_PARTICLES * mpi_size / 1024.0 / 1024.0 / time_total;
+        double bandwidth    = per_particle * NUM_PARTICLES / 1024.0 / 1024.0 / time_total * mpi_size;
         printf("Bandwidth: %.2fMB/s, total time: %.4f, lock: %.4f, io: %.4f, release: %.4f\n", bandwidth,
                time_total, time_lock, time_io, time_release);
     }
