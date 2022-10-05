89daaff691c3f97495ef1dbe1c21b2e11a112540
Houjun Tang <htang4@lbl.gov>
2022-10-05 11:38:45 -0700

    Fix the issue with vpic scaling

--- src/api/pdc_region/pdc_region_transfer.c
+++ src/api/pdc_region/pdc_region_transfer.c
@@ -1982,10 +1982,11 @@ PDCregion_transfer_wait(pdcid_t transfer_request_id)
         remove_local_transfer_request(transfer_request->obj_pointer, transfer_request_id);
     }
     else {
-        printf("PDC Client PDCregion_transfer_status attempt to check status for inactive transfer request @ "
-               "line %d\n",
-               __LINE__);
-        ret_value = FAIL;
+        // metadata is freed with previous wait (e.g. with posix consistency)
+        /* printf("PDC Client PDCregion_transfer_status attempt to check status for inactive transfer request @ " */
+        /*        "line %d\n", */
+        /*        __LINE__); */
+        ret_value = SUCCEED;
     }
 
 done:
