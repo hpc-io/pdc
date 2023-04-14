8121e3ce28fffcc50a3cc315c48f8826f929619d
Wei Zhang <zhangwei217245@gmail.com>
2023-04-13 23:16:11 -0500

    llsm tiff import test

---
+++ tools/llsm/parallelReadTiff.h
@@ -0,0 +1,22 @@
+#ifndef PARALLELREADTIFF_H
+#define PARALLELREADTIFF_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <limits.h>
+
+#include "tiffio.h"
+#include "omp.h"
+// #include "commons/generic/pdc_generic.h"
+
+typedef struct {
+    uint64_t *range, size_t length
+} parallel_tiff_range_t;
+
+void parallel_TIFF_load(char *fileName, void **tiff_ptr, uint8_t flipXY, parallel_tiff_range_t *strip_range);
+
+#endif // PARALLELREADTIFF_H
