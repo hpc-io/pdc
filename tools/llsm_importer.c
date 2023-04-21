12cbd7fb63f4a91786e6411e9b9bd727397e9f8e
Wei Zhang <zhangwei217245@gmail.com>
2023-04-20 23:58:08 -0500

    add timing

--- tools/llsm_importer.c
+++ tools/llsm_importer.c
@@ -55,23 +55,33 @@ void
 on_image(image_file_info_t *image_info, img_scan_callback_args_t *args)
 {
     print_image_file_info(image_info);
-    
+
     char *dirname = (char *)args->input;
-    char filepath[256];
+    char  filepath[256];
     // calling tiff loading process.
-    void *tiff      = NULL;
-    int   i         = 0;
+    void *          tiff = NULL;
+    int             i    = 0;
+    struct timespec start, end;
+    double          duration;
 
     // check if the path ends with a forward slash
     if (dirname[strlen(dirname) - 1] != '/') {
         strcat(dirname, "/"); // add a forward slash to the end of the path
     }
 
-    strcpy(filepath, dirname); // copy the directory path to the file path
+    strcpy(filepath, dirname);              // copy the directory path to the file path
     strcat(filepath, image_info->filename); // concatenate the file name to the file path
 
+    clock_gettime(CLOCK_MONOTONIC, &start); // start timing the operation
 
     parallel_TIFF_load(filepath, &tiff, 1, NULL);
+
+    clock_gettime(CLOCK_MONOTONIC, &end); // end timing the operation
+
+    duration = (end.tv_sec - start.tv_sec) * 1e9 +
+               (end.tv_nsec - start.tv_nsec); // calculate duration in nanoseconds
+
+    printf("Read %s Done! Time taken: %.4f seconds\n", filepath, duration / 1e9);
 
     if (!tiff)
         return;
@@ -88,14 +98,14 @@ int
 main(int argc, char *argv[])
 {
 
-    char *file_name = NULL;
+    char *                   file_name = NULL;
     img_scan_callback_args_t callback_args;
     // parse console argument
     int parse_code = parse_console_args(argc, argv, &file_name);
     if (parse_code) {
         return parse_code;
     }
-    char* directory_path = dirname(strdup(file_name));
+    char *directory_path = dirname(strdup(file_name));
 
     // print file name for validating purpose
     printf("Filename: %s\n", file_name ? file_name : "(none)");
