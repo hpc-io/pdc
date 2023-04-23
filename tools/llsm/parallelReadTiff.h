f57c882d164f2610ddb89e0619f8cf0c64c8a527
Wei Zhang <zhangwei217245@gmail.com>
2023-04-23 17:16:16 -0500

    llsm tiff importer 1st version: read csv and import tiff files to PDC, adding metadata available in CSV files and TIFF loader

--- tools/llsm/parallelReadTiff.h
+++ tools/llsm/parallelReadTiff.h
@@ -15,6 +15,20 @@ typedef struct {
     size_t    length;
 } parallel_tiff_range_t;
 
-void parallel_TIFF_load(char *fileName, void **tiff_ptr, uint8_t flipXY, parallel_tiff_range_t *strip_range);
+typedef struct {
+    uint64_t x;
+    uint64_t y;
+    uint64_t z;
+    uint64_t bits;
+    uint64_t startSlice;
+    uint64_t stripeSize;
+    uint64_t is_imageJ;
+    uint64_t imageJ_Z;
+    void *   tiff_ptr;
+    size_t   tiff_size;
+} image_info_t;
+
+
+void parallel_TIFF_load(char *fileName, uint8_t flipXY, parallel_tiff_range_t *strip_range, image_info_t **image_info_t);
 
 #endif // PARALLELREADTIFF_H
