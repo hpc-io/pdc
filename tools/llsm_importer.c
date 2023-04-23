5c0c37dce9131d05b05444b59cfe2421a347c63c
Wei Zhang <zhangwei217245@gmail.com>
2023-04-23 17:16:16 -0500

    llsm tiff importer 1st version: read csv and import tiff files to PDC, adding metadata available in CSV files and TIFF loader

--- tools/llsm_importer.c
+++ tools/llsm_importer.c
@@ -4,10 +4,10 @@
 #include <getopt.h>
 #include <time.h>
 
-// #define ENABLE_MPI 1
+#define ENABLE_MPI 1
 
 #ifdef ENABLE_MPI
-// #include "mpi.h"
+#include "mpi.h"
 #endif
 
 #include "pdc.h"
@@ -15,10 +15,19 @@
 // #include "pdc_client_connect.h"
 
 #include "llsm/parallelReadTiff.h"
-#include "llsm/imageListReader.h"
+#include "llsm/pdc_list.h"
+#include "llsm/csvReader.h"
 #include <libgen.h>
 
-int     rank = 0, size = 1;
+typedef struct llsm_importer_args_t {
+    char *        directory_path;
+    csv_header_t *csv_header;
+} llsm_importer_args_t;
+
+int rank = 0, size = 1;
+
+pdcid_t pdc, cont_prop, cont, obj_prop;
+pdcid_t pdc_id_g = 0, cont_prop_g = 0, cont_id_g = 0, obj_prop_g = 0;
 
 int
 parse_console_args(int argc, char *argv[], char **file_name)
@@ -41,84 +50,175 @@ parse_console_args(int argc, char *argv[], char **file_name)
 }
 
 void
-print_image_file_info(const image_file_info_t *image_info)
+import_to_pdc(const image_info_t *image_info, const csv_cell_t *fileName_cell)
 {
-    printf("Filepath: %s\n", image_info->filepath);
-    printf("Filename: %s\n", image_info->filename);
-    printf("Stage X (um): %.2f\n", image_info->stageX_um_);
-    printf("Stage Y (um): %.2f\n", image_info->stageY_um_);
-    printf("Stage Z (um): %.2f\n", image_info->stageZ_um_);
-    printf("Objective X (um): %.2f\n", image_info->objectiveX_um_);
-    printf("Objective Y (um): %.2f\n", image_info->objectiveY_um_);
-    printf("Objective Z (um): %.2f\n", image_info->objectiveZ_um_);
-}
-
-// void
-// import_to_pdc(const image_file_info_t *image_info) {
-
-
-//     PDCprop_set_obj_dims(obj_prop_g, cur_ndims, (uint64_t *)cur_dims);
-//     PDCprop_set_obj_type(obj_prop_g, cur_type);
-//     PDCprop_set_obj_time_step(obj_prop_g, 0);
-//     PDCprop_set_obj_user_id(obj_prop_g, getuid());
-//     PDCprop_set_obj_app_name(obj_prop_g, app_name);
-// }
-
-void
-on_image(image_file_info_t *image_info, img_scan_callback_args_t *args)
-{
-    print_image_file_info(image_info);
-
-    char *dirname = (char *)args->input;
-    char  filepath[256];
-    // calling tiff loading process.
-    void *          tiff = NULL;
-    int             i    = 0;
     struct timespec start, end;
     double          duration;
+
+    clock_gettime(CLOCK_MONOTONIC, &start); // start timing the operation
+
+    pdcid_t cur_obj_prop_g = PDCprop_create(PDC_OBJ_CREATE, pdc_id_g);
+
+    psize_t ndims   = 3;
+    // FIXME: we should support uint64_t. 
+    uint64_t dims[3] = {image_info->x, image_info->y, image_info->z};
+    // FIXME: we should change the ndims parameter to psize_t type.
+    PDCprop_set_obj_dims(obj_prop_g, (PDC_int_t)ndims, dims);
+    PDCprop_set_obj_type(obj_prop_g, PDC_FLOAT);
+    PDCprop_set_obj_time_step(obj_prop_g, 0);
+    PDCprop_set_obj_user_id(obj_prop_g, getuid());
+    PDCprop_set_obj_app_name(obj_prop_g, "LLSM");
+
+    // create object
+    // FIXME: There are many attributes currently in one file name, 
+    // and we should do some research to see what would be a good object name for each image.
+    pdcid_t cur_obj_g = PDCobj_create(cont_id_g, fileName_cell->field_value, cur_obj_prop_g);
+
+    // write data to object
+    pdcid_t local_region     = PDCregion_create(ndims, 0, image_info->tiff_size);
+    pdcid_t remote_region    = PDCregion_create(ndims, 0, image_info->tiff_size);
+    pdcid_t transfer_request = PDCregion_transfer_create(image_info->tiff_ptr, PDC_WRITE, cur_obj_g, local_region, remote_region);
+    PDCregion_transfer_start(transfer_request);
+    PDCregion_transfer_wait(transfer_request);
+
+    // add metadata tags based on the csv row
+    csv_cell_t *cell = fileName_cell;
+    while (cell != NULL) {
+        char *field_name = cell->header->field_name;
+        char data_type   = cell->header->field_type;
+        char *field_value = cell->field_value;
+        switch(data_type) {
+            case 'i':
+                int value = atoi(field_value);
+                PDCobj_put_tag(cur_obj_g, field_name, &value, sizeof(int));
+                break;
+            case 'f':
+                double value = atof(field_value);
+                PDCobj_put_tag(cur_obj_g, field_name, &value, sizeof(double));
+                break;
+            case 's':
+                PDCobj_put_tag(cur_obj_g, field_name, field_value, sizeof(char) * strlen(field_value));
+                break;
+            default:
+                break;
+        }
+        cell = cell->next;
+    }
+
+    // add extra metadata tags based on the image_info struct
+    PDCobj_put_tag(cur_obj_g, "x", &image_info->x, sizeof(uint64_t));
+    PDCobj_put_tag(cur_obj_g, "y", &image_info->y, sizeof(uint64_t));
+    PDCobj_put_tag(cur_obj_g, "z", &image_info->z, sizeof(uint64_t));
+    PDCobj_put_tag(cur_obj_g, "bits", &image_info->bits, sizeof(uint64_t));
+    PDCobj_put_tag(cur_obj_g, "startSlice", &image_info->startSlice, sizeof(uint64_t));
+    PDCobj_put_tag(cur_obj_g, "stripeSize", &image_info->stripeSize, sizeof(uint64_t));
+    PDCobj_put_tag(cur_obj_g, "is_imageJ", &image_info->is_imageJ, sizeof(uint64_t));
+    PDCobj_put_tag(cur_obj_g, "imageJ_Z", &image_info->imageJ_Z, sizeof(uint64_t));
+
+    // close object
+    PDCobj_close(cur_obj_g);
+
+    // get timing
+    clock_gettime(CLOCK_MONOTONIC, &end); // end timing the operation
+    duration = (end.tv_sec - start.tv_sec) * 1e9 +
+               (end.tv_nsec - start.tv_nsec); // calculate duration in nanoseconds
+
+    printf("[Rank %4d]create object %s Done! Time taken: %.4f seconds\n", rank, fileName_cell->field_value, duration / 1e9);
+
+}
+
+void
+on_csv_row(csv_row_t *row, llsm_importer_args_t *llsm_args)
+{
+    csv_print_row(row, 1);
+
+    char *dirname = strdup(llsm_args->directory_path);
+    char  filepath[256];
+    // calling tiff loading process.
+    image_info_t *  image_info = NULL;
+    int             i          = 0;
+    struct timespec start, end;
+    double          duration;
+    // Filepath,Filename,StageX_um_,StageY_um_,StageZ_um_,ObjectiveX_um_,ObjectiveY_um_,ObjectiveZ_um_
+
+    // get the file name from the csv row
+    csv_cell_t *fileName_cell = csv_get_field_value_by_name(row, llsm_args->csv_header, "Filename");
 
     // check if the path ends with a forward slash
     if (dirname[strlen(dirname) - 1] != '/') {
         strcat(dirname, "/"); // add a forward slash to the end of the path
     }
 
-    strcpy(filepath, dirname);              // copy the directory path to the file path
-    strcat(filepath, image_info->filename); // concatenate the file name to the file path
+    strcpy(filepath, dirname);                         // copy the directory path to the file path
+    strcat(filepath, fileName_cell->field_value); // concatenate the file name to the file path
 
     clock_gettime(CLOCK_MONOTONIC, &start); // start timing the operation
 
-    parallel_TIFF_load(filepath, &tiff, 1, NULL);
+    parallel_TIFF_load(filepath, 1, NULL, &image_info);
 
     clock_gettime(CLOCK_MONOTONIC, &end); // end timing the operation
 
     duration = (end.tv_sec - start.tv_sec) * 1e9 +
                (end.tv_nsec - start.tv_nsec); // calculate duration in nanoseconds
 
-    printf("Read %s Done! Time taken: %.4f seconds\n", filepath, duration / 1e9);
+    printf("[Rand %4d]Read %s Done! Time taken: %.4f seconds\n", rank, filepath, duration / 1e9);
 
-    if (!tiff)
+    if (image_info == NULL || image_info->tiff_ptr == NULL) {
         return;
+    }
 
     printf("first few bytes ");
     for (i = 0; i < 10; i++) {
-        printf("%d ", ((uint8_t *)tiff)[i]);
+        printf("%d ", ((uint8_t *)image_info->tiff_ptr)[i]);
     }
     printf("\n");
-    free(tiff);
+
+    // import the image to PDC
+    import_to_pdc(image_info, fileName_cell);
+
+    // free the image info
+    free(image_info->tiff_ptr);
+    free(image_info);
+    free(dirname);
+
+    // free the csv row
+    csv_free_row(row);
+}
+
+void
+read_txt(char *txtFileName, PDC_LIST *list)
+{
+    FILE *file = fopen(txtFileName, "r");
+
+    if (file == NULL) {
+        printf("Error: could not open file %s\n", txtFileName);
+        return;
+    }
+    char buffer[1024];
+    // Read the lines of the file
+    while (fgets(buffer, sizeof(buffer), file)) {
+        pdc_list_add(list, strdup(buffer));
+    }
 }
 
 int
 main(int argc, char *argv[])
 {
 
-// #ifdef ENABLE_MPI
-//     MPI_Init(&argc, &argv);
-//     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-//     MPI_Comm_size(MPI_COMM_WORLD, &size);
-// #endif
+#ifdef ENABLE_MPI
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+#endif
 
-    char *                   file_name = NULL;
-    img_scan_callback_args_t callback_args;
+    char *                file_name       = NULL;
+    PDC_LIST *            list            = pdc_list_new();
+    char *                csv_line        = NULL;
+    int                   num_row_read    = 0;
+    csv_header_t *        csv_header      = NULL;
+    csv_row_t *           csv_row         = NULL;
+    llsm_importer_args_t *llsm_args       = NULL;
+    char *                csv_field_types = {'s', 's', 'f', 'f', 'f', 'f', 'f', 'f'};
     // parse console argument
     int parse_code = parse_console_args(argc, argv, &file_name);
     if (parse_code) {
@@ -130,13 +230,73 @@ main(int argc, char *argv[])
     printf("Filename: %s\n", file_name ? file_name : "(none)");
     printf("Directory: %s\n", directory_path ? directory_path : "(none)");
 
-    callback_args.input = (void *)directory_path;
-    scan_image_list(file_name, &on_image, &callback_args);
+    // create a pdc
+    pdc_id_g = PDCinit("pdc");
 
+    // create a container property
+    cont_prop_g = PDCprop_create(PDC_CONT_CREATE, pdc);
+    if (cont_prop <= 0)
+        printf("Fail to create container property @ line  %d!\n", __LINE__);
 
-// #ifdef ENABLE_MPI
-//     MPI_Finalize();
-// #endif
+    // create a container
+    cont = PDCcont_create("c1", cont_prop);
+    if (cont <= 0)
+        printf("Fail to create container @ line  %d!\n", __LINE__);
+
+    // Rank 0 reads the filename list and distribute data to other ranks
+    if (rank == 0) {
+        read_txt(file_name, list);
+#ifdef ENABLE_MPI
+        // broadcast the number of lines
+        int num_lines = pdc_list_size(list);
+        MPI_Bcast(&num_lines, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        // broadcast the file names
+        PDC_LIST_ITERATOR *iter = pdc_list_iterator_new(list);
+        while (pdc_list_iterator_has_next(iter)) {
+            char *csv_line = (char *)pdc_list_iterator_next(iter);
+            MPI_Bcast(csv_line, 256, MPI_CHAR, 0, MPI_COMM_WORLD);
+        }
+#endif
+    }
+    else {
+#ifdef ENABLE_MPI
+        // other ranks receive the number of files
+        int num_lines;
+        MPI_Bcast(&num_lines, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        // receive the file names
+        int i;
+        for (i = 0; i < num_lines; i++) {
+            csv_line = (char *)malloc(256 * sizeof(char));
+            MPI_Bcast(csv_line, 256, MPI_CHAR, 0, MPI_COMM_WORLD);
+            pdc_list_add(list, csv_line);
+        }
+#endif
+    }
+    // parse the csv
+    csv_table_t *csv_table = csv_parse_list(list, csv_field_types);
+    if (csv_table == NULL) {
+        printf("Fail to parse csv file @ line  %d!\n", __LINE__);
+        return -1;
+    }
+    llsm_args                 = (llsm_importer_args_t *)malloc(sizeof(llsm_importer_args_t));
+    llsm_args->directory_path = directory_path;
+    llsm_args->csv_header     = csv_table->first_header;
+
+    // go through the csv table
+    csv_row_t *current_row = csv_table->first_row;
+    while (current_row != NULL) {
+        if (num_row_read % size == rank) {
+            on_csv_row(current_row, llsm_args);
+        }
+        num_row_read++;
+        current_row = current_row->next;
+    }
+
+    csv_free_table(csv_table);
+
+#ifdef ENABLE_MPI
+    MPI_Finalize();
+#endif
 
     return 0;
 }
