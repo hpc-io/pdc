52817c17085c61e97a3d38baa25736b8191d88cb
Houjun Tang <htang4@lbl.gov>
2023-05-23 15:22:28 -0700

    Fix Issue #85, server segfault when another client application with different number of ranks connects to it

--- src/server/include/pdc_client_server_common.h
+++ src/server/include/pdc_client_server_common.h
@@ -282,6 +282,7 @@ typedef struct {
 typedef struct {
     int32_t client_id;
     int32_t nclient;
+    int32_t is_init;
     char    client_addr[ADDR_MAX];
 } client_test_connect_args;
 
@@ -688,6 +689,7 @@ typedef struct {
 typedef struct {
     uint32_t    client_id;
     int32_t     nclient;
+    int         is_init;
     hg_string_t client_addr;
 } client_test_connect_in_t;
 
@@ -2136,6 +2138,11 @@ hg_proc_client_test_connect_in_t(hg_proc_t proc, void *data)
         return ret;
     }
     ret = hg_proc_int32_t(proc, &struct_data->nclient);
+    if (ret != HG_SUCCESS) {
+        // HG_LOG_ERROR("Proc error");
+        return ret;
+    }
+    ret = hg_proc_int32_t(proc, &struct_data->is_init);
     if (ret != HG_SUCCESS) {
         // HG_LOG_ERROR("Proc error");
         return ret;
