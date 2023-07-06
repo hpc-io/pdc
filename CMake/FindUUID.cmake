7610d720847cc2e5e4a2ebbeb30fd7c171200dfe
Wei Zhang <zhangwei217245@lbl.gov>
2023-07-06 16:26:58 -0400

    fix uuid

--- CMake/FindUUID.cmake
+++ CMake/FindUUID.cmake
@@ -7,17 +7,17 @@
 # UUID_INCLUDE_DIRS - The UUID include directory
 # UUID_LIBRARIES - The libraries needed to use UUID
 
-find_package(UUID QUIET HINTS $ENV{LIBUUID_HOME} $ENV{LIBUUID_DIR} $ENV{LIBUUID_ROOT} $ENV{LIBUUID_PATH} $ENV{UUID_HOME} $ENV{UUID_DIR} $ENV{UUID_ROOT} $ENV{UUID_PATH})
-if (UUID_FOUND)
-    find_path(UUID_INCLUDE_DIR uuid/uuid.h HINTS ${UUID_DIR})
-    find_library(UUID_LIBRARY NAMES uuid)
+find_package(PkgConfig)
+pkg_check_modules(UUID REQUIRED uuid)
 
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(UUID DEFAULT_MSG UUID_LIBRARY UUID_INCLUDE_DIR)
-    set(UUID_INCLUDE_DIRS ${UUID_INCLUDE_DIR})
-    set(UUID_LIBRARIES ${UUID_LIBRARY})
-else(UUID_FOUND)
-    set(UUID_LIBRARIES "")
-endif (UUID_FOUND)
+find_path(UUID_INCLUDE_DIR uuid/uuid.h HINTS ${UUID_INCLUDE_DIRS})
+find_library(UUID_LIBRARY NAMES uuid HINTS ${UUID_LIBRARY_DIRS})
+
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set UUID_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(UUID DEFAULT_MSG
+                                  UUID_INCLUDE_DIR UUID_LIBRARY)
 
 mark_as_advanced(UUID_INCLUDE_DIR UUID_LIBRARY)
