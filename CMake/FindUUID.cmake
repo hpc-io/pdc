021a4bbefba5df28d92349226eec170dc1d757bb
Wei Zhang <zhangwei217245@lbl.gov>
2023-07-10 15:36:36 -0400

    fix mercury in CMake

--- CMake/FindUUID.cmake
+++ CMake/FindUUID.cmake
@@ -7,12 +7,25 @@
 # UUID_INCLUDE_DIRS - The UUID include directory
 # UUID_LIBRARIES - The libraries needed to use UUID
 
+# - Try to find UUID
+# Once done this will define
+#  UUID_FOUND - System has UUID
+#  UUID_INCLUDE_DIRS - The UUID include directories
+#  UUID_LIBRARIES - The libraries needed to use UUID
+
 find_package(PkgConfig)
-pkg_check_modules(UUID REQUIRED uuid)
+pkg_check_modules(PC_UUID uuid)
 
-find_path(UUID_INCLUDE_DIR uuid/uuid.h HINTS ${UUID_INCLUDE_DIRS})
-find_library(UUID_LIBRARY NAMES uuid HINTS ${UUID_LIBRARY_DIRS})
+find_path(UUID_INCLUDE_DIR uuid/uuid.h
+  HINTS ${PC_UUID_INCLUDEDIR} ${PC_UUID_INCLUDE_DIRS}
+  PATHS /usr/local/include /usr/include)
 
+find_library(UUID_LIBRARY NAMES uuid
+  HINTS ${PC_DRC_LIBDIR} ${PC_DRC_LIBRARY_DIRS}
+  PATHS /usr/local/lib64 /usr/local/lib /usr/lib64 /usr/lib)
+
+set(UUID_INCLUDE_DIRS ${UUID_INCLUDE_DIR})
+set(UUID_LIBRARIES ${UUID_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set UUID_FOUND to TRUE
