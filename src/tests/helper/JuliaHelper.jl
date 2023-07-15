85a7b7d0b72e2806fc5c100fa190e61e17e51e51
Wei Zhang <zhangwei217245@lbl.gov>
2023-07-15 00:45:33 -0400

    new files

---
+++ src/tests/helper/JuliaHelper.jl
@@ -0,0 +1,86 @@
+module JuliaHelper
+
+export my_julia_func
+export generate_attribute_occurrences
+export generate_incremental_associations
+
+using Distributions
+
+function my_julia_func(input::Int64)
+    result = [input, input + 1, input + 2, input + 3]
+    return result
+end
+
+function generate_attribute_occurrences(num_attributes::Int64, num_objects::Int64, distribution::String, s::Float64=1.0)
+    if distribution == "uniform"
+        dist = Multinomial(num_objects, fill(1.0/num_attributes, num_attributes))
+        return Int64.(rand(dist, 1))
+    elseif distribution == "zipf"
+        dist = Zipf(s, num_attributes)
+        occurrences = Int64.(rand(dist, num_objects))
+        
+        # count the number of occurrences of each attribute
+        attribute_counts = zeros(Int64, num_attributes)
+        for i in 1:num_objects
+            attribute_counts[occurrences[i]] += 1
+        end
+        
+        return attribute_counts
+    elseif distribution == "pareto"
+        dist = Pareto(s, 1.0)  # Pareto distribution with shape parameter s and scale 1.0
+        occurrences = Int64.(round.(occurrences .* num_objects ./ sum(occurrences)))
+        
+        # Due to rounding, the sum might not be exactly num_objects. 
+        # Add or subtract the difference to a random element.
+        diff = num_objects - sum(occurrences)
+        occurrences[rand(1:num_attributes)] += diff
+        
+        return occurrences
+    elseif distribution == "normal"
+        dist = Normal(0.0, s)
+        occurrences = Int64.(round.(occurrences .* num_objects ./ sum(occurrences)))
+        
+        # Due to rounding, the sum might not be exactly num_objects. 
+        # Add or subtract the difference to a random element.
+        diff = num_objects - sum(occurrences)
+        occurrences[rand(1:num_attributes)] += diff
+        
+        return occurrences
+    elseif distribution == "exponential"
+        dist = Exponential(s)
+        occurrences = Int64.(round.(occurrences .* num_objects ./ sum(occurrences)))
+        
+        # Due to rounding, the sum might not be exactly num_objects. 
+        # Add or subtract the difference to a random element.
+        diff = num_objects - sum(occurrences)
+        occurrences[rand(1:num_attributes)] += diff
+        
+        return occurrences
+    else
+        error("Invalid distribution: " * distribution)
+    end
+end
+
+
+function generate_incremental_associations(total_objects::Int64, total_groups::Int64, num_attributes::Int64)
+    # Error checking: Ensure that the number of attributes does not exceed the total number of groups
+    if num_attributes > total_groups
+        error("Number of attributes cannot be greater than the total number of groups")
+    end
+
+    # Generate an array to store the number of objects for each attribute
+    attribute_objects = Vector{Int64}(undef, num_attributes)
+    
+    # Calculate the increment value based on the total number of objects and the number of attributes
+    increment_value = ceil(Int64, total_objects / num_attributes)
+
+    # Assign an incremental number of objects to each attribute
+    for i = 1:num_attributes
+        # Ensure the number of objects does not exceed the total number of objects
+        attribute_objects[i] = min(i * increment_value, total_objects)
+    end
+
+    return attribute_objects
+end
+
+end  # module MyModule
---
+++ src/tests/helper/julia_helper_loader.c
@@ -0,0 +1,100 @@
+#include "julia_helper_loader.h"
+
+void
+jl_load_module(const char *mod_name)
+{
+    /* get julia helper directory */
+    const char *julia_module_dir = getenv("PDC_JULIA_MODULE_DIR");
+    if (julia_module_dir == NULL || strlen(julia_module_dir) == 0) {
+        // try to get it from PWD
+        julia_module_dir = getenv("PWD");
+    }
+    printf("Warning: PDC_JULIA_MODULE_DIR is not set, fallback to PWD!\n");
+    if (julia_module_dir == NULL || strlen(julia_module_dir) == 0) {
+        // No way to find julia module directory
+        printf("Error: Not able to find Julia module directory!\n");
+        exit(-1);
+    }
+    printf("Julia module directory: %s\n", julia_module_dir);
+
+    /* get julia helper path */
+    char *julia_module_path = malloc(strlen(julia_module_dir) + strlen(mod_name) + 5);
+    strcpy(julia_module_path, julia_module_dir);
+    strcat(julia_module_path, "/");
+    strcat(julia_module_path, mod_name);
+    strcat(julia_module_path, ".jl");
+    printf("Julia module path: %s\n", julia_module_path);
+    /* get include command */
+    char include_cmd[strlen(julia_module_path) + 30];
+    sprintf(include_cmd, "Base.include(Main, \"%s\")", julia_module_path);
+    /* get using command */
+    char using_cmd[strlen(mod_name) + 15];
+    sprintf(using_cmd, "using Main.%s", mod_name);
+
+    jl_eval_string(include_cmd);
+    jl_eval_string(using_cmd);
+}
+
+jl_value_t *
+run_jl_function(const char *mod_name, const char *fun_name, jl_fn_args_t *args)
+{
+    char module_eval_cmd[strlen(mod_name) + 15];
+    sprintf(module_eval_cmd, "Main.%s", mod_name);
+    jl_module_t *  JuliaModule = (jl_module_t *)jl_eval_string(module_eval_cmd);
+    jl_function_t *jl_func     = jl_get_function(JuliaModule, fun_name);
+    return jl_call(jl_func, args->args, args->nargs);
+}
+
+void
+run_jl_get_int64_array(const char *mod_name, const char *fun_name, jl_fn_args_t *args, int64_t **arr_ptr,
+                       size_t *arr_len)
+{
+    jl_value_t *ret = run_jl_function(mod_name, fun_name, args);
+    JL_GC_PUSH1(&ret);
+    if (jl_typeis(ret, jl_apply_array_type((jl_value_t *)jl_int64_type, 1))) {
+        jl_array_t *ret_array = (jl_array_t *)ret;
+        int64_t *   data      = (int64_t *)jl_array_data(ret_array);
+        *arr_ptr              = data;
+        *arr_len              = jl_array_len(ret_array);
+    }
+    else {
+        printf("Error: return value is not an int64 array!\n");
+    }
+    JL_GC_POP();
+}
+
+void
+run_jl_get_float64_array(const char *mod_name, const char *fun_name, jl_fn_args_t *args, double **arr_ptr,
+                         size_t *arr_len)
+{
+    jl_value_t *ret = run_jl_function(mod_name, fun_name, args);
+    JL_GC_PUSH1(&ret);
+    if (jl_typeis(ret, jl_apply_array_type((jl_value_t *)jl_float64_type, 1))) {
+        jl_array_t *ret_array = (jl_array_t *)ret;
+        double *    data      = (double *)jl_array_data(ret_array);
+        *arr_ptr              = data;
+        *arr_len              = jl_array_len(ret_array);
+    }
+    else {
+        printf("Error: return value is not a float64 array!\n");
+    }
+    JL_GC_POP();
+}
+
+void
+run_jl_get_string_array(const char *mod_name, const char *fun_name, jl_fn_args_t *args, char ***arr_ptr,
+                        size_t *arr_len)
+{
+    jl_value_t *ret = run_jl_function(mod_name, fun_name, args);
+    JL_GC_PUSH1(&ret);
+    if (jl_typeis(ret, jl_apply_array_type((jl_value_t *)jl_string_type, 1))) {
+        jl_array_t *ret_array = (jl_array_t *)ret;
+        char **     data      = (char **)jl_array_data(ret_array);
+        *arr_ptr              = data;
+        *arr_len              = jl_array_len(ret_array);
+    }
+    else {
+        printf("Error: return value is not a string array!\n");
+    }
+    JL_GC_POP();
+}
