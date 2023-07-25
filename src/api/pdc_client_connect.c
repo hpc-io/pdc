7d5b129cc7ff4698d5a7d5fc51f0b2178418e871
Wei Zhang <zhangwei217245@lbl.gov>
2023-07-25 13:15:51 -0400

    update

--- src/api/pdc_client_connect.c
+++ src/api/pdc_client_connect.c
@@ -8818,7 +8818,7 @@ PDC_Client_search_obj_ref_through_dart(dart_hash_algo_t hash_algo, char *query_s
         // thread_param->hashset = hashset;
         // thpool_add_work(query_pool, (void *)dart_perform_on_one_server_thread, (void *)thread_param);
 
-        int dart_status = dart_perform_on_one_server(serverId, &input_param, &hashset);
+        // int dart_status = dart_perform_on_one_server(serverId, &input_param, &hashset);
         if (omit_request == 1 && set_num_entries(hashset) > 0) {
             break;
         }
@@ -8845,13 +8845,13 @@ PDC_Client_search_obj_ref_through_dart(dart_hash_algo_t hash_algo, char *query_s
 
     // done:
     // thpool_destroy(query_pool);
-    // free(k_query);
-    // free(v_query);
-    // if (affix != NULL)
-    //     free(affix);
-    // if (tok != NULL)
-    //     free(tok);
-    // set_free(hashset);
+    free(k_query);
+    free(v_query);
+    if (affix != NULL)
+        free(affix);
+    if (tok != NULL)
+        free(tok);
+    set_free(hashset);
 
     return ret;
 }
