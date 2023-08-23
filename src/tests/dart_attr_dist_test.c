be1a5075ccba78f9d669363c18094a67bd377cc4
Wei Zhang <zhangwei217245@gmail.com>
2023-08-23 00:59:11 -0400

    update

--- src/tests/dart_attr_dist_test.c
+++ src/tests/dart_attr_dist_test.c
@@ -18,8 +18,11 @@
 
 #include "pdc.h"
 #include "pdc_client_connect.h"
-#include "julia_helper_loader.h"
 
+// #define PDC_ENABLE_JULIA 1
+
+#ifdef PDC_ENABLE_JULIA
+#include "julia_helper_loader.h"
 #define JULIA_HELPER_NAME "JuliaHelper"
 // only define the following once, in an executable (not in a shared library) if you want fast
 // code.
@@ -55,6 +58,7 @@ generate_attribute_occurrences(int64_t num_attr, int64_t num_obj, const char *di
     /* run generate_attribute_occurrences  with parameters */
     run_jl_get_int64_array(JULIA_HELPER_NAME, "generate_attribute_occurrences", args, arr, len);
 }
+#endif
 
 void
 generate_attr_obj_association(int64_t num_attr, int64_t num_obj, int64_t **arr, size_t *len)
@@ -98,16 +102,20 @@ main(int argc, char *argv[])
     char suffix_query[48];
     char infix_query[48];
 
-    // jl_module_list_t modules = {.julia_modules = (char *[]){JULIA_HELPER_NAME}, .num_modules = 1};
-    // init_julia(&modules);
+#ifdef PDC_ENABLE_JULIA
+    jl_module_list_t modules = {.julia_modules = (char *[]){JULIA_HELPER_NAME}, .num_modules = 1};
+    init_julia(&modules);
+#endif
 
     if (rank == 0) {
         // calling julia helper to get the array.
 
+#ifdef PDC_ENABLE_JULIA
+        generate_incremental_associations(total_num_attr, total_num_obj, total_num_attr, &attr_2_obj_array,
+                                          &arr_len);
+#else
         generate_attr_obj_association(total_num_attr, total_num_obj, &attr_2_obj_array, &arr_len);
-
-        // generate_incremental_associations(total_num_attr, total_num_obj, total_num_attr, &attr_2_obj_array,
-        //                                   &arr_len);
+#endif
 
         // generate_attribute_occurrences(total_num_attr, total_num_obj, "uniform", &attr_2_obj_array,
         // &arr_len);
@@ -337,7 +345,9 @@ main(int argc, char *argv[])
     // free attr_2_obj_array
     free(attr_2_obj_array);
 
-    // close_julia();
+#ifdef PDC_ENABLE_JULIA
+    close_julia();
+#endif
 
 #ifdef ENABLE_MPI
     MPI_Finalize();
