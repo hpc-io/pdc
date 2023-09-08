43909906a3f30ce43cf1c9c215f4107296e1e5a5
Wei Zhang <zhangwei217245@lbl.gov>
2023-09-07 21:59:14 -0400

    update

--- scripts/kvtag_query_scale_mpi/gen_script.sh
+++ scripts/kvtag_query_scale_mpi/gen_script.sh
@@ -22,28 +22,34 @@ MAX_NODE=512
 MAX_ATTR=1024
 MAX_ATTRLEN=1000
 
-PROG_BASENAME=kvtag_query_scale_col
+PROG_BASENAME=kvqry
 
 for (( i = 1; i <= $MAX_NODE; i*=2 )); do
+    mkdir -p $i
     for (( j = 0; j <= 1; j+=1 )); do
-        mkdir -p $i
-        JOBNAME=${PROG_BASENAME}_${i}
-        TARGET=./$i/$JOBNAME.sbatch.${j}
-        cp template.sh $TARGET
-        sed -i "s/JOBNAME/${JOBNAME}/g"           $TARGET
-        sed -i "s/NODENUM/${i}/g"           $TARGET
-        sed -i "s/MPHYSICALCORE/${MAX_PYSICAL_CORE}/g"           $TARGET
-        sed -i "s/MHYPERTHREADING/${MAX_HYPERTHREADING}/g"           $TARGET
-        sed -i "s/N_SERVER_PROC/${NUM_SERVER_PROC_PER_NODE}/g"           $TARGET
-        sed -i "s/N_CLIENT_PROC/${NUM_CLIENT_PROC_PER_NODE}/g"           $TARGET
-        sed -i "s/NTHREAD_PER_SPROC/${NUM_THREAD_PER_SERVER_PROC}/g"           $TARGET
-        sed -i "s/NTHREAD_PER_CPROC/${NUM_THREAD_PER_CLIENT_PROC}/g"           $TARGET
-        sed -i "s/PROJNAME/${PROJECT_NAME}/g"           $TARGET
-        sed -i "s/USING_DART/${j}/g"           $TARGET
-        if [[ "$i" -gt "4" ]]; then
-            sed -i "s/REG//g"           $TARGET
-        else
-            sed -i "s/DBG//g"           $TARGET
-        fi
+        for (( q = 0; q < 4; q+=1 )); do
+            for (( c = 0; c < 2; c+=1 )); do
+                JOBNAME=${PROG_BASENAME}_${i}_${j}_${q}_${c}
+                TARGET=./$i/$JOBNAME.sbatch.${j}
+                cp template.sh $TARGET
+                sed -i "s/JOBNAME/${JOBNAME}/g"           $TARGET
+                sed -i "s/NODENUM/${i}/g"           $TARGET
+                sed -i "s/MPHYSICALCORE/${MAX_PYSICAL_CORE}/g"           $TARGET
+                sed -i "s/MHYPERTHREADING/${MAX_HYPERTHREADING}/g"           $TARGET
+                sed -i "s/N_SERVER_PROC/${NUM_SERVER_PROC_PER_NODE}/g"           $TARGET
+                sed -i "s/N_CLIENT_PROC/${NUM_CLIENT_PROC_PER_NODE}/g"           $TARGET
+                sed -i "s/NTHREAD_PER_SPROC/${NUM_THREAD_PER_SERVER_PROC}/g"           $TARGET
+                sed -i "s/NTHREAD_PER_CPROC/${NUM_THREAD_PER_CLIENT_PROC}/g"           $TARGET
+                sed -i "s/PROJNAME/${PROJECT_NAME}/g"           $TARGET
+                sed -i "s/USING_DART/${j}/g"           $TARGET
+                sed -i "s/QUERY_TYPE/${q}/g"           $TARGET
+                sed -i "s/COMMUNICATION_TYPE/${c}/g"           $TARGET
+                if [[ "$i" -gt "4" ]]; then
+                    sed -i "s/REG//g"           $TARGET
+                else
+                    sed -i "s/DBG//g"           $TARGET
+                fi
+            done
+        done
     done
 done
