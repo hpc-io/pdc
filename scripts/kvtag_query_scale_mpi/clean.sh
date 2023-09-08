d68456ee45c30a22c8fcb783ed1cc2598563be82
Wei Zhang <zhangwei217245@lbl.gov>
2023-09-07 22:10:14 -0400

    update

--- scripts/kvtag_query_scale_mpi/clean.sh
+++ scripts/kvtag_query_scale_mpi/clean.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: $0 <clean_dir>"
+    exit 1
+fi
+
+CLEAN_DIR=$1
 MAX_NODE=512
 
-for (( i = 1; i <= $MAX_NODE; i*=2 )); do
+# if CLEAN_DIR is set to '1', then clean all the directories named with numbers, otherwise, clean the sbatch script only
+find ./ -name   "*.sbatch*" -delete
 
-    rm -rf $i/*
+if [[ "$CLEAN_DIR" -eq "1" ]]; then
+    for (( i = 1; i <= $MAX_NODE; i*=2 )); do
 
-done
+        rm -rf $i/*
+
+    done
+fi
