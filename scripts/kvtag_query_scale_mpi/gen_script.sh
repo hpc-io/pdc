4170bb4b3b56fff0e2df0a7592d849ecf0af7ac8
Wei Zhang <zhangwei217245@gmail.com>
2023-10-01 07:17:43 -0400

    job generator

--- scripts/kvtag_query_scale_mpi/gen_script.sh
+++ scripts/kvtag_query_scale_mpi/gen_script.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-PROJECT_NAME=$1
+if [[ "$#" -ne 2 ]]; then
+    echo "Usage: $0 <min_node> <proj_name>"
+    exit 1
+fi
+
+# test if $1 is a number
+re='^[0-9]+$'
+if ! [[ $1 =~ $re ]] ; then
+    echo "Error: min_node is not a number" >&2; exit 1
+fi
+
+if [[ "$1" -lt "1" ]]; then
+    echo "Error: min_node should be larger than 0"
+    exit 1
+fi
+
+if [[ "$1" -gt "512" ]]; then
+    echo "Error: min_node should be smaller than 512"
+    exit 1
+fi
+
+MIN_NODE=$1
+MAX_NODE=512
+MAX_ATTR=1024
+MAX_ATTRLEN=1000
+
+
 
 # Per node configuration of your HPC system. 
 MAX_PYSICAL_CORE=128
@@ -12,20 +38,26 @@ NUM_THREAD_PER_SERVER_PROC=2
 NUM_THREAD_PER_CLIENT_PROC=2
 
 
+PROJECT_NAME=$2
+
+
 # Designated number of processes for server anc client on each node
 # (this should be associated with -n option in srun)
+TOTAL_NUM_CLIENT_PROC=$((128 * MIN_NODE))
 NUM_SERVER_PROC_PER_NODE=1
-NUM_CLIENT_PROC_PER_NODE=120
+NUM_CLIENT_PROC_PER_NODE=$((TOTAL_NUM_CLIENT_PROC))
 
-
-MAX_NODE=512
-MAX_ATTR=1024
-MAX_ATTRLEN=1000
 
 PROG_BASENAME=kvqry
 
-for (( i = 1; i <= $MAX_NODE; i*=2 )); do
+for (( i = $MIN_NODE; i <= $MAX_NODE; i*=2 )); do
     mkdir -p $i
+    NUM_CLIENT_PROC_PER_NODE=$((TOTAL_NUM_CLIENT_PROC/i))
+    AVAIL_CLIENT_THREAD_CORES=$((MAX_PYSICAL_CORE * MAX_HYPERTHREADING - NUM_SERVER_PROC_PER_NODE * NUM_THREAD_PER_SERVER_PROC))
+    AVAIL_CLIENT_PHYSICAL_CORES=$((AVAIL_CLIENT_THREAD_CORES / NUM_THREAD_PER_CLIENT_PROC))
+    if [[  $(( NUM_CLIENT_PROC_PER_NODE > AVAIL_CLIENT_PHYSICAL_CORES )) -eq 1  ]]; then
+        NUM_CLIENT_PROC_PER_NODE=$((AVAIL_CLIENT_PHYSICAL_CORES - 2))
+    fi
     for (( j = 0; j <= 1; j+=1 )); do
         for (( q = 0; q < 4; q+=1 )); do
             for (( c = 0; c < 2; c+=1 )); do
