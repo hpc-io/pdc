90fc7b47259bf5caa07bc6d1c50620488b19666a
Wei Zhang <zhangwei217245@gmail.com>
2023-10-13 15:05:55 -0400

    update docker run and build script

--- .docker/publish_dev_base.sh
+++ .docker/publish_dev_base.sh
@@ -11,60 +11,63 @@ IMG_NS=$1
 arch=$(uname -m)
 ARCH_CODE=""
 
-case $arch in
-    x86_64)
-        ARCH_CODE="amd64"
-        echo "You are running on x86_64 (AMD64) architecture."
-        ;;
-    arm64 | aarch64)
-        ARCH_CODE="arm64v8"
-        echo "You are running on ARM64 (AArch64) architecture."
-        ;;
-    i386 | i686)
-        ARCH_CODE="i386"
-        echo "You are running on x86 (32-bit) architecture."
-        ;;
-    arm*)
-        ARCH_CODE="arm32v7"
-        echo "You are running on ARM (32-bit) architecture."
-        ;;
-    ppc64le)
-        ARCH_CODE="ppc64le"
-        echo "You are running on PowerPC (64-bit little-endian) architecture."
-        ;;
-    s390x)
-        ARCH_CODE="s390x"
-        echo "You are running on IBM Z (s390x) architecture."
-        ;;
-    *)
-        echo "Unknown or unsupported architecture: $arch"
-        exit 1
-        ;;
-esac
-
-DEFAULT_WORKSPACE=/workspaces/pdc
-
-docker build -t ${IMG_NS}/pdc_dev_base:latest-${ARCH_CODE} -f .docker/base.Dockerfile --build-arg ARCH=${ARCH_CODE}/ .
-docker push ${IMG_NS}/pdc_dev_base:latest-${ARCH_CODE}
-
 if [ -z "$2" ] || [ "$2" -eq 0 ]; then
+
+    case $arch in
+        x86_64)
+            ARCH_CODE="amd64"
+            echo "You are running on x86_64 (AMD64) architecture."
+            ;;
+        arm64 | aarch64)
+            ARCH_CODE="arm64v8"
+            echo "You are running on ARM64 (AArch64) architecture."
+            ;;
+        i386 | i686)
+            ARCH_CODE="i386"
+            echo "You are running on x86 (32-bit) architecture."
+            ;;
+        arm*)
+            ARCH_CODE="arm32v7"
+            echo "You are running on ARM (32-bit) architecture."
+            ;;
+        ppc64le)
+            ARCH_CODE="ppc64le"
+            echo "You are running on PowerPC (64-bit little-endian) architecture."
+            ;;
+        s390x)
+            ARCH_CODE="s390x"
+            echo "You are running on IBM Z (s390x) architecture."
+            ;;
+        *)
+            echo "Unknown or unsupported architecture: $arch"
+            exit 1
+            ;;
+    esac
+
+    DEFAULT_WORKSPACE=/workspaces/pdc
+
+    docker build -t ${IMG_NS}/pdc_dev_base:latest-${ARCH_CODE} -f .docker/base.Dockerfile --build-arg ARCH=${ARCH_CODE}/ .
+    docker push ${IMG_NS}/pdc_dev_base:latest-${ARCH_CODE}
     exit 0
+else
+    echo "Processing manifest..."
+    # Process manifest
+    # arch_strings=("amd64" "arm64v8" "i386" "arm32v7" "ppc64le" "s390x")
+    arch_strings=("amd64" "arm64v8")
+    manifest_args=()
+    for arch in "${arch_strings[@]}"; do
+        echo "Processing architecture: $arch"
+        if [[  "$arch" == "$ARCH_CODE" ]]; then
+            echo "Skipping current architecture: $arch"
+            continue
+        fi
+        # Your logic for each architecture goes here...
+        docker pull ${IMG_NS}/pdc_dev_base:latest-${arch}
+        manifest_args+=("--amend" "${IMG_NS}/pdc_dev_base:latest-${arch}")
+    done
+
+    docker manifest create ${IMG_NS}/pdc_dev_base:latest ${IMG_NS}/pdc_dev_base:latest-${ARCH_CODE} ${manifest_args[@]}
+    docker manifest push ${IMG_NS}/pdc_dev_base:latest
 fi
 
-# Process manifest
-# arch_strings=("amd64" "arm64v8" "i386" "arm32v7" "ppc64le" "s390x")
-arch_strings=("amd64" "arm64v8")
-manifest_args=()
-for arch in "${arch_strings[@]}"; do
-    echo "Processing architecture: $arch"
-    if [[  "$arch" == "$ARCH_CODE" ]]; then
-        echo "Skipping current architecture: $arch"
-        continue
-    fi
-    # Your logic for each architecture goes here...
-    docker pull ${IMG_NS}/pdc_dev_base:latest-${arch}
-    manifest_args+=("--amend" "${IMG_NS}/pdc_dev_base:latest-${arch}")
-done
 
-docker manifest create ${IMG_NS}/pdc_dev_base:latest ${IMG_NS}/pdc_dev_base:latest-${ARCH_CODE} ${manifest_args[@]}
-docker manifest push ${IMG_NS}/pdc_dev_base:latest
