84f71a4972616112a277d34ac5d4a012b6931c83
Jean Luca Bez <jeanlucabez@gmail.com>
2023-10-30 10:56:26 -0700

    Update dependencies-macos.sh

--- .github/workflows/dependencies-macos.sh
+++ .github/workflows/dependencies-macos.sh
@@ -5,7 +5,7 @@ set -eu -o pipefail
 brew install open-mpi autoconf automake libtool ossp-uuid
 
 # libfabric
-wget https://github.com/ofiwg/libfabric/archive/refs/tags/v1.12.1.tar.gz
+wget https://github.com/ofiwg/libfabric/archive/refs/tags/v1.15.2.tar.gz
 tar xf v1.12.1.tar.gz
 cd libfabric-1.12.1
 ./autogen.sh
@@ -17,7 +17,7 @@ cd ..
 # Mercury
 git clone --recursive https://github.com/mercury-hpc/mercury.git
 cd mercury
-git checkout v2.1.0
+git checkout v2.2.0
 mkdir build && cd build
 cmake ../  -DCMAKE_C_COMPILER=gcc -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=ON -DNA_USE_OFI=ON -DNA_USE_SM=OFF -DMERCURY_USE_CHECKSUMS=OFF -DNA_OFI_TESTING_PROTOCOL=sockets
 make -j2 && sudo make install
