b02a9700c7a076360e569f70fe937fa45ea52426
Jean Luca Bez <jlbez@lbl.gov>
2023-12-18 11:37:55 -0800

    fixes for MacOS and new CI to prevent future breaks (#164)
    * include MacOS in CI
    * include dependencies
    * replace TCP with sockets
    * add libuuid
    * fix find UUID to correctly locate files in Ubuntu and MacOS
    * fix random segfault in strdup + malloc due to wrong allocation
    * ensure consistent use of defined variables
    * change transport for MacOS tests
    * update env
    * update dependencies-macos.sh
    * replace found to TRUE/FALSE
    * update documentation with timeout and MacOS specifics
    * fix git link to avoid authentication
    * change transport for tests
    * configure network for MacOS tests
    
    ---------
    
    Co-authored-by: github-actions <github-actions[bot]@users.noreply.github.com>

---
+++ .github/workflows/dependencies-macos.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=FALSE
+
+brew install open-mpi automake
+
+# libfabric
+wget https://github.com/ofiwg/libfabric/archive/refs/tags/v1.15.2.tar.gz
+tar xf v1.15.2.tar.gz
+cd libfabric-1.15.2
+./autogen.sh
+./configure --disable-usnic --disable-mrail --disable-rstream --disable-perf --disable-efa --disable-psm2 --disable-psm --disable-verbs --disable-shm --disable-static --disable-silent-rules
+make -j2 && sudo make install
+make check
+cd ..
+
+# Mercury
+git clone --recursive https://github.com/mercury-hpc/mercury.git
+cd mercury
+git checkout v2.2.0
+mkdir build && cd build
+cmake ../  -DCMAKE_C_COMPILER=gcc -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=ON -DNA_USE_OFI=ON -DNA_USE_SM=OFF -DMERCURY_USE_CHECKSUMS=OFF -DNA_OFI_TESTING_PROTOCOL=sockets
+make -j2 && sudo make install
+ctest
