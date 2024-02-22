eefc13bcc3ad3ac5634afa0d74d957cba253e64c
Wei Zhang <zhangwei217245@lbl.gov>
2024-02-22 19:22:04 +0000

    update

--- src/tools/meta_json/metadata_json_importer.c
+++ src/tools/meta_json/metadata_json_importer.c
@@ -216,6 +216,7 @@ int
 finalize_importer(MD_JSON_ARGS *md_json_args)
 {
     pdc_importer_args_t *pdc_args = (pdc_importer_args_t *)md_json_args->processor_args;
+    printf("finalize_importer\n");
     // finalize PDC related things
     // close pdc
     if (PDCclose(pdc_args->pdc) < 0) {
