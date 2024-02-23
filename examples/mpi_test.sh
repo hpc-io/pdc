38d5f77bfb6e8948e1ed56443f5b7a70c5f24007
Hyunju Oh <oh.693@osu.edu>
2024-02-23 18:48:21 -0500

    Removing gres option for ctest (#182)
    * Removing gres option for ctest
    * Removing gres option from scripts
    * Update check for core
    
    ---------
    
    Co-authored-by: Hyunju Oh <hjoh16@login15.chn.perlmutter.nersc.gov>
    Co-authored-by: Jean Luca Bez <jlbez@lbl.gov>

--- examples/mpi_test.sh
+++ examples/mpi_test.sh
@@ -4,8 +4,8 @@
 # defined and points to the MPI libraries used by the linker (e.g. -L<path -lmpi)
 
 extra_cmd=""
-if [[ "$HOSTNAME" == "cori"* || "$HOSTNAME" == "nid"* ]]; then
-    extra_cmd="--mem=25600 --cpu_bind=cores --gres=craynetwork:1 --overlap "
+if [[ "$NERSC_HOST" == "perlmutter" ]]; then
+    extra_cmd="--mem=25600 --cpu_bind=cores --overlap "
 fi
 
 if [ $# -lt 1 ]; then echo "missing test argument" && exit -1 ; fi
