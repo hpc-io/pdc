2e307585efcca3d148e08b5848998f34bc3f7e31
Houjun Tang <htang4@lbl.gov>
2024-03-12 17:09:49 -0700

    Minor adjustment to query benchmark

--- src/tests/kvtag_query_multi.c
+++ src/tests/kvtag_query_multi.c
@@ -61,7 +61,7 @@ assign_work_to_rank(int rank, int size, int nwork, int *my_count, int *my_start)
 void
 print_usage(char *name)
 {
-    printf("%s n_obj n_tag_per_obj selectivity n_condition n_round is_using_dart\n", name);
+    printf("%s n_obj n_tag_per_obj selectivity n_condition n_round query_only is_using_dart\n", name);
     printf("Summary: This test will create n_obj objects, and add n_tag_per_obj * 4 tags to each on \n"
            "it will perform n_round queries against the tags with specified selectivity, \n"
            "each query from each client should get the entire result set.\n");
@@ -73,6 +73,7 @@ print_usage(char *name)
     printf("               selectivity * n_tag_per_obj must also be an integer and dividible by 2.\n");
     printf("  n_condition: number of conditions in a query, currently only supports 4, 8, 16\n");
     printf("  n_round: number of rounds to perform the same query\n");
+    printf("  query_only: 1 for query without create, 0 for create all objs and tags\n");
     printf("  is_using_dart: 1 for using dart, 0 for not using dart\n");
 }
 
@@ -100,7 +101,7 @@ main(int argc, char *argv[])
     pdcid_t *   obj_ids;
     int         n_obj, my_obj, my_obj_s;
     int         proc_num = 1, my_rank = 0, ntag_per_obj, n_condition, round, is_using_dart;
-    int         i, j, int_val, nsel;
+    int         i, j, int_val, nsel, query_only;
     char        obj_name[1024], tag_name[1024], query[1024], value[1024];
     char        tmp_str1[1024], tmp_str2[1024], tmp_str3[1024], tmp_str4[1024];
     char *      char_ptr;
@@ -127,9 +128,15 @@ main(int argc, char *argv[])
     selectivity   = atof(argv[3]);
     n_condition   = atoi(argv[4]);
     round         = atoi(argv[5]);
-    is_using_dart = atoi(argv[6]);
+    query_only    = atoi(argv[6]);
+    is_using_dart = atoi(argv[7]);
 
     nsel = (int)n_obj * selectivity / 100.0;
+
+    if (my_rank == 0)
+        printf("n_obj = %d, ntag_per_obj = %d, selectivity = %lf, n_condition = %d, round = %d, "
+               "query_only = %d, is_using_dart = %d\n", 
+               n_obj, ntag_per_obj, selectivity, n_condition, round, query_only, is_using_dart);
 
     // create a pdc
     pdc = PDCinit("pdc");
@@ -158,7 +165,10 @@ main(int argc, char *argv[])
     obj_ids = (pdcid_t *)calloc(my_obj, sizeof(pdcid_t));
     for (i = 0; i < my_obj; i++) {
         sprintf(obj_name, "obj%d", my_obj_s + i);
-        obj_ids[i] = PDCobj_create(cont, obj_name, obj_prop);
+        if (query_only == 0)
+            obj_ids[i] = PDCobj_create(cont, obj_name, obj_prop);
+        else
+            obj_ids[i] = PDCobj_open(obj_name, pdc);
         if (obj_ids[i] <= 0)
             printf("Fail to create object @ line  %d!\n", __LINE__);
     }
@@ -173,114 +183,116 @@ main(int argc, char *argv[])
     if (my_rank == 0)
         printf("I will add %d tags to each obj\n", ntag_per_obj);
 
-    // Add tag to objects just created
-    for (i = 0; i < my_obj; i++) {
-        for (j = 0; j < ntag_per_obj; j++) {
+    if (query_only == 0) {
+        // Add tag to objects just created
+        for (i = 0; i < my_obj; i++) {
+            for (j = 0; j < ntag_per_obj; j++) {
 
-            // int
-            sprintf(tag_name, "int_%d", j);
-            int_val     = i + my_obj_s;
-            kvtag.name  = tag_name;
-            kvtag.value = (void *)&int_val;
-            kvtag.type  = PDC_INT;
-            kvtag.size  = sizeof(int);
+                // int
+                sprintf(tag_name, "int_%d", j);
+                int_val     = i + my_obj_s;
+                kvtag.name  = tag_name;
+                kvtag.value = (void *)&int_val;
+                kvtag.type  = PDC_INT;
+                kvtag.size  = sizeof(int);
 
-            if (is_using_dart) {
-                sprintf(value, "%d", int_val);
-                if (PDC_Client_insert_obj_ref_into_dart(hash_algo, kvtag.name, value, ref_type,
-                                                        (uint64_t)obj_ids[i]) < 0) {
-                    printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                if (is_using_dart) {
+                    sprintf(value, "%d", int_val);
+                    if (PDC_Client_insert_obj_ref_into_dart(hash_algo, kvtag.name, value, ref_type,
+                                                            (uint64_t)obj_ids[i]) < 0) {
+                        printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                    }
                 }
-            }
-            else {
-                if (ntag_per_obj < 20)
-                    println("Rank %d: obj %llu [%s] [%d], len %d", my_rank, obj_ids[i], kvtag.name,
-                            *((int *)kvtag.value), kvtag.size);
-                if (PDCobj_put_tag(obj_ids[i], kvtag.name, kvtag.value, kvtag.type, kvtag.size) < 0) {
-                    printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                else {
+                    if (ntag_per_obj < 20)
+                        println("Rank %d: obj %llu [%s] [%d], len %d", my_rank, obj_ids[i], kvtag.name,
+                                *((int *)kvtag.value), kvtag.size);
+                    if (PDCobj_put_tag(obj_ids[i], kvtag.name, kvtag.value, kvtag.type, kvtag.size) < 0) {
+                        printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                    }
                 }
-            }
 
-            // float
-            sprintf(tag_name, "float_%d", j);
-            float_val   = (float)i + my_obj_s;
-            kvtag.name  = tag_name;
-            kvtag.value = (void *)&float_val;
-            kvtag.type  = PDC_FLOAT;
-            kvtag.size  = sizeof(float);
+                // float
+                sprintf(tag_name, "float_%d", j);
+                float_val   = (float)i + my_obj_s;
+                kvtag.name  = tag_name;
+                kvtag.value = (void *)&float_val;
+                kvtag.type  = PDC_FLOAT;
+                kvtag.size  = sizeof(float);
 
-            if (is_using_dart) {
-                sprintf(value, "%f", float_val);
-                if (PDC_Client_insert_obj_ref_into_dart(hash_algo, kvtag.name, value, ref_type,
-                                                        (uint64_t)obj_ids[i]) < 0) {
-                    printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                if (is_using_dart) {
+                    sprintf(value, "%f", float_val);
+                    if (PDC_Client_insert_obj_ref_into_dart(hash_algo, kvtag.name, value, ref_type,
+                                                            (uint64_t)obj_ids[i]) < 0) {
+                        printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                    }
                 }
-            }
-            else {
-                if (ntag_per_obj < 20)
-                    println("Rank %d: obj %llu [%s] [%f], len %d", my_rank, obj_ids[i], kvtag.name,
-                            *((float *)kvtag.value), kvtag.size);
-                if (PDCobj_put_tag(obj_ids[i], kvtag.name, kvtag.value, kvtag.type, kvtag.size) < 0) {
-                    printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                else {
+                    if (ntag_per_obj < 20)
+                        println("Rank %d: obj %llu [%s] [%f], len %d", my_rank, obj_ids[i], kvtag.name,
+                                *((float *)kvtag.value), kvtag.size);
+                    if (PDCobj_put_tag(obj_ids[i], kvtag.name, kvtag.value, kvtag.type, kvtag.size) < 0) {
+                        printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                    }
                 }
-            }
 
-            // double
-            sprintf(tag_name, "double_%d", j);
-            double_val  = (double)i + my_obj_s;
-            kvtag.name  = tag_name;
-            kvtag.value = (void *)&double_val;
-            kvtag.type  = PDC_DOUBLE;
-            kvtag.size  = sizeof(double);
+                // double
+                sprintf(tag_name, "double_%d", j);
+                double_val  = (double)i + my_obj_s;
+                kvtag.name  = tag_name;
+                kvtag.value = (void *)&double_val;
+                kvtag.type  = PDC_DOUBLE;
+                kvtag.size  = sizeof(double);
 
-            if (is_using_dart) {
-                sprintf(value, "%lf", double_val);
-                if (PDC_Client_insert_obj_ref_into_dart(hash_algo, kvtag.name, value, ref_type,
-                                                        (uint64_t)obj_ids[i]) < 0) {
-                    printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                if (is_using_dart) {
+                    sprintf(value, "%lf", double_val);
+                    if (PDC_Client_insert_obj_ref_into_dart(hash_algo, kvtag.name, value, ref_type,
+                                                            (uint64_t)obj_ids[i]) < 0) {
+                        printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                    }
                 }
-            }
-            else {
-                if (ntag_per_obj < 20)
-                    println("Rank %d: obj %llu [%s] [%lf], len %d", my_rank, obj_ids[i], kvtag.name,
-                            *((double *)kvtag.value), kvtag.size);
-                if (PDCobj_put_tag(obj_ids[i], kvtag.name, kvtag.value, kvtag.type, kvtag.size) < 0) {
-                    printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                else {
+                    if (ntag_per_obj < 20)
+                        println("Rank %d: obj %llu [%s] [%lf], len %d", my_rank, obj_ids[i], kvtag.name,
+                                *((double *)kvtag.value), kvtag.size);
+                    if (PDCobj_put_tag(obj_ids[i], kvtag.name, kvtag.value, kvtag.type, kvtag.size) < 0) {
+                        printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                    }
                 }
-            }
 
-            // string
-            sprintf(tag_name, "string_%d", j);
-            sprintf(tmp_str1, "%010d", i + my_obj_s);
-            kvtag.name  = tag_name;
-            kvtag.value = (void *)tmp_str1;
-            kvtag.type  = PDC_STRING;
-            kvtag.size  = strlen(tmp_str1) + 1;
+                // string
+                sprintf(tag_name, "string_%d", j);
+                sprintf(tmp_str1, "%010d", i + my_obj_s);
+                kvtag.name  = tag_name;
+                kvtag.value = (void *)tmp_str1;
+                kvtag.type  = PDC_STRING;
+                kvtag.size  = strlen(tmp_str1) + 1;
 
-            if (is_using_dart) {
-                sprintf(value, "%s", tmp_str1);
-                if (PDC_Client_insert_obj_ref_into_dart(hash_algo, kvtag.name, value, ref_type,
-                                                        (uint64_t)obj_ids[i]) < 0) {
-                    printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                if (is_using_dart) {
+                    sprintf(value, "%s", tmp_str1);
+                    if (PDC_Client_insert_obj_ref_into_dart(hash_algo, kvtag.name, value, ref_type,
+                                                            (uint64_t)obj_ids[i]) < 0) {
+                        printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                    }
                 }
-            }
-            else {
-                if (ntag_per_obj < 20)
-                    println("Rank %d: obj %llu [%s] [%s], len %d", my_rank, obj_ids[i], kvtag.name,
-                            (char *)kvtag.value, kvtag.size);
-                if (PDCobj_put_tag(obj_ids[i], kvtag.name, kvtag.value, kvtag.type, kvtag.size) < 0) {
-                    printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                else {
+                    if (ntag_per_obj < 20)
+                        println("Rank %d: obj %llu [%s] [%s], len %d", my_rank, obj_ids[i], kvtag.name,
+                                (char *)kvtag.value, kvtag.size);
+                    if (PDCobj_put_tag(obj_ids[i], kvtag.name, kvtag.value, kvtag.type, kvtag.size) < 0) {
+                        printf("fail to add a kvtag to obj %d\n", i + my_obj_s);
+                    }
                 }
             }
         }
-    }
 
 #ifdef ENABLE_MPI
-    MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
 #endif
-    if (my_rank == 0)
-        printf("Finished adding %d tags\n", n_obj * ntag_per_obj);
-    fflush(stdout);
+        if (my_rank == 0)
+            printf("Added %d tags\n", n_obj * ntag_per_obj);
+        fflush(stdout);
+    } // End query_only = 0
 
     if (n_condition == 4) {
         sprintf(tmp_str1, "string_0=string(%010d)", nsel);
@@ -337,39 +349,43 @@ main(int argc, char *argv[])
     if (my_rank == 0)
         println("Rank %d: query kvtag [%s] [%s]", my_rank, query_kvtag.name, (char *)query_kvtag.value);
 
+    for (j = 0; j < round; j++) {
+        if (my_rank == 0)
+            println("Round %d", j);
 #ifdef ENABLE_MPI
-    MPI_Barrier(MPI_COMM_WORLD);
-    stime = MPI_Wtime();
-    if (PDC_Client_query_kvtag_mpi(&query_kvtag, &nres, &pdc_ids, MPI_COMM_WORLD) < 0) {
+        MPI_Barrier(MPI_COMM_WORLD);
+        stime = MPI_Wtime();
+        if (PDC_Client_query_kvtag_mpi(&query_kvtag, &nres, &pdc_ids, MPI_COMM_WORLD) < 0) {
 #else
-    if (PDC_Client_query_kvtag(&query_kvtag, &nres, &pdc_ids) < 0) {
+        if (PDC_Client_query_kvtag(&query_kvtag, &nres, &pdc_ids) < 0) {
 #endif
-        printf("fail to query kvtag [%s] with rank %d\n", query_kvtag.name, my_rank);
-    }
+            printf("fail to query kvtag [%s] with rank %d\n", query_kvtag.name, my_rank);
+        }
 
 #ifdef ENABLE_MPI
-    MPI_Barrier(MPI_COMM_WORLD);
-    total_time = MPI_Wtime() - stime;
+        MPI_Barrier(MPI_COMM_WORLD);
+        total_time = MPI_Wtime() - stime;
 
-    if (my_rank == 0) {
-        if (nres != nsel) {
-            println("Query result %d does not match expected %d", nres, nsel);
+        if (my_rank == 0) {
+            if (nres != nsel) {
+                println("Query result %d does not match expected %d", nres, nsel);
+                for (i = 0; i < nres; i++)
+                    printf("%llu\n", pdc_ids[i]);
+            }
+            else
+                println("Total time to query %d objects with tag: %.5f", nres, total_time);
+        }
+#else
+        if (nres != nsel)
+            println("Query found %d objects does not match %d", nres, nsel);
+        else
+            println("Query found %d objects", nres);
+
+        if (nres < 20)
             for (i = 0; i < nres; i++)
                 printf("%llu\n", pdc_ids[i]);
-        }
-        else
-            println("Total time to query %d objects with tag: %.5f", nres, total_time);
-    }
-#else
-    if (nres != nsel)
-        println("Query found %d objects does not match %d", nres, nsel);
-    else
-        println("Query found %d objects", nres);
-
-    if (nres < 20)
-        for (i = 0; i < nres; i++)
-            printf("%llu\n", pdc_ids[i]);
 #endif
+    }
 
     // close a container
     if (PDCcont_close(cont) < 0)
