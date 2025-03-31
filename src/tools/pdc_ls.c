098c9738b368608aa632c07a0aa776ccc606389c
Noah Lewis <47840925+TheAssembler1@users.noreply.github.com>
2025-03-31 13:00:35 -0400

    Malloc correct size for pdc_obj_metadata_pkg (#237)
    Co-authored-by: Jean Luca Bez <jlbez@lbl.gov>

--- src/tools/pdc_ls.c
+++ src/tools/pdc_ls.c
@@ -313,12 +313,11 @@ do_transfer_request_metadata(int pdc_server_size_input, char *checkpoint)
         ptr += sizeof(int);
         for (i = 0; i < n_objs; ++i) {
             if (metadata_server_objs) {
-                metadata_server_objs_end->next =
-                    (pdc_obj_metadata_pkg *)malloc(sizeof(pdc_obj_region_metadata));
-                metadata_server_objs_end = metadata_server_objs_end->next;
+                metadata_server_objs_end->next = (pdc_obj_metadata_pkg *)malloc(sizeof(pdc_obj_metadata_pkg));
+                metadata_server_objs_end       = metadata_server_objs_end->next;
             }
             else {
-                metadata_server_objs     = (pdc_obj_metadata_pkg *)malloc(sizeof(pdc_obj_region_metadata));
+                metadata_server_objs     = (pdc_obj_metadata_pkg *)malloc(sizeof(pdc_obj_metadata_pkg));
                 metadata_server_objs_end = metadata_server_objs;
             }
 
