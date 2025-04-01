b44e0211429c4bf743a5308e9094c635f236a8fa
Jean Luca Bez <jlbez@lbl.gov>
2025-04-01 10:24:11 -0700

    fix mercury version to match the docs

--- .github/workflows/dependencies-linux.sh
+++ .github/workflows/dependencies-linux.sh
@@ -18,7 +18,7 @@ cd ..
 # Mercury
 git clone --recursive https://github.com/mercury-hpc/mercury.git
 cd mercury
-git checkout v2.1.0
+git checkout v2.2.0
 mkdir build && cd build
 cmake ../  -DCMAKE_C_COMPILER=gcc -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=ON -DNA_USE_OFI=ON -DNA_USE_SM=OFF -DMERCURY_USE_CHECKSUMS=OFF
 make -j2 && sudo make install
