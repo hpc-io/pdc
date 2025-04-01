84d8c45c9bedb9a994d9b02031736faec480cd67
Jean Luca Bez <jlbez@lbl.gov>
2025-04-01 10:32:38 -0700

    try fix

--- .github/workflows/dependencies-linux.sh
+++ .github/workflows/dependencies-linux.sh
@@ -20,6 +20,6 @@ git clone --recursive https://github.com/mercury-hpc/mercury.git
 cd mercury
 git checkout v2.2.0
 mkdir build && cd build
-cmake ../  -DCMAKE_C_COMPILER=gcc -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=ON -DNA_USE_OFI=ON -DNA_USE_SM=OFF -DMERCURY_USE_CHECKSUMS=OFF
+cmake ../  -DCMAKE_C_COMPILER=gcc -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=ON -DNA_USE_OFI=ON -DNA_USE_SM=OFF -DMERCURY_USE_CHECKSUMS=OFF -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 make -j2 && sudo make install
 ctest
