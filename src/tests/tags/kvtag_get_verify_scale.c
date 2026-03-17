684bf327e625c40f7166c11078581584be97cba6
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2026-03-17 13:44:56 -0400

    Refactor kvtag-get-verify test code

--- src/tests/tags/kvtag_get_verify_scale.c
+++ src/tests/tags/kvtag_get_verify_scale.c
@@ -62,7 +62,7 @@ void
 print_usage(char *name)
 {
     // required parameters: n_obj and n_query
-    LOG_JUST_PRINT("%s n_obj n_query\n", name);
+    LOG_JUST_PRINT("%s n_obj n_tag\n", name);
 }
 
 int
@@ -70,8 +70,8 @@ main(int argc, char *argv[])
 {
     pdcid_t     pdc, cont_prop, cont;
     pdcid_t *   obj_ids;
-    int         n_obj, n_query, my_obj, my_obj_s, my_query, my_query_s;
-    int         query_1percent = 0;
+    int         n_obj, n_query, my_obj, my_obj_s, n_tag, n_tag_s;
+    int         obj_1percent = 0;
     int         proc_num, my_rank, i;
     char        obj_name[128];
     double      stime, total_time, percent_time;
@@ -105,21 +105,16 @@ main(int argc, char *argv[])
         PGOTO_DONE(FAIL);
     }
 
-    assign_work_to_rank(my_rank, proc_num, n_query, &my_query, &my_query_s);
+    assign_work_to_rank(my_rank, proc_num, n_query, &n_tag, &n_tag_s);
     assign_work_to_rank(my_rank, proc_num, n_obj, &my_obj, &my_obj_s);
 
-    query_1percent = my_query / 100;
+    obj_1percent = my_obj / 100;
 
     if (my_rank == 0)
-        LOG_INFO("Open %d obj, query %d tags\n", my_obj, my_query);
+        LOG_INFO("Open %d obj, query %d tags\n", my_obj, n_tag);
 
     // create a pdc
     pdc = PDCinit("pdc");
-
-    // create a container property
-    cont_prop = PDCprop_create(PDC_CONT_CREATE, pdc);
-    if (cont_prop <= 0)
-        PGOTO_ERROR(FAIL, "Failed to create container property");
 
     // Open the existing container
     cont = PDCcont_open("c1", pdc);
@@ -142,12 +137,12 @@ main(int argc, char *argv[])
             PGOTO_ERROR(FAIL, "Failed to open object");
 
         // progress reporting for object opening
-        if (i > 0 && query_1percent > 0 && i % query_1percent == 0) {
+        if (i > 0 && obj_1percent > 0 && i % obj_1percent == 0) {
 #ifdef ENABLE_MPI
             MPI_Barrier(MPI_COMM_WORLD);
             percent_time = MPI_Wtime() - stime;
             if (my_rank == 0) {
-                int    current_percentage              = i / query_1percent;
+                int    current_percentage              = i / obj_1percent;
                 int    estimated_current_object_number = n_obj / 100 * current_percentage;
                 double tps                             = estimated_current_object_number / percent_time;
                 LOG_INFO("[OBJ PROGRESS %3d%% ] %11d objects, %7.2f seconds, TPS: %10.2f \n",
@@ -169,46 +164,18 @@ main(int argc, char *argv[])
     // Setup kvtag for queries
     kvtag.name = "Group";
 
-    values = (void **)calloc(my_query, sizeof(void *));
+    values = (void **)calloc(n_tag, sizeof(void *));
 
 #ifdef ENABLE_MPI
     MPI_Barrier(MPI_COMM_WORLD);
     stime = MPI_Wtime();
 #endif
-    for (i = 0; i < my_query; i++) {
+    for (i = 0; i < n_tag; i++) {
         if (PDCobj_get_tag(obj_ids[i], kvtag.name, (void *)&values[i], (void *)&value_type,
                            (void *)&value_size) < 0)
-            PGOTO_ERROR(FAIL, "Failed to get a kvtag from o%d\n", i + my_query_s);
+            PGOTO_ERROR(FAIL, "Failed to get a kvtag from o%d\n", i + n_tag_s);
 
-        if (i % query_1percent == 0) {
-#ifdef ENABLE_MPI
-            MPI_Barrier(MPI_COMM_WORLD);
-            percent_time = MPI_Wtime() - stime;
-            if (my_rank == 0) {
-                int    current_percentage             = i / query_1percent;
-                int    estimated_current_query_number = n_obj / 100 * current_percentage;
-                double tps                            = estimated_current_query_number / percent_time;
-                LOG_INFO("[QRY PROGRESS %3d%% ] %11d queries, %7.2f seconds, TPS: %10.2f \n",
-                         current_percentage, estimated_current_query_number, percent_time, tps);
-            }
-#endif
-        }
-    }
-
-#ifdef ENABLE_MPI
-    MPI_Barrier(MPI_COMM_WORLD);
-    total_time = MPI_Wtime() - stime;
-#endif
-    if (my_rank == 0)
-        LOG_INFO("Total time to retrieve 1 tag from %11d objects: %7.2f , throughput %10.2f \n", n_query,
-                 total_time, n_query / total_time);
-
-    // `kvtag_add_get_scale' program adds tags with value = i + my_add_tag_s
-    // calculate and checking the expected value
-    for (i = 0; i < my_query; i++) {
-        // when tags were added, each rank tagged objects starting from my_add_tag_s
-        // for verification, we need to determine which rank added the tag and its offset
-        int expected_value = i + my_query_s;  // Assuming tags were added in order starting from 0
+        int expected_value = i + n_tag_s;  // Assuming tags were added in order starting from 0
 
         // count successful and failed verifications instead of immediate error
         if (*(int *)(values[i]) == expected_value) {
@@ -218,11 +185,19 @@ main(int argc, char *argv[])
             // log first 10 failures for debugging
             if (verified_fail <= 10) {
                 LOG_ERROR("Verification failed for obj%d: expected %d, got %d\n",
-                          i + my_query_s, expected_value, *(int *)(values[i]));
+                          i + n_tag_s, expected_value, *(int *)(values[i]));
             }
         }
         free(values[i]);
     }
+
+#ifdef ENABLE_MPI
+    MPI_Barrier(MPI_COMM_WORLD);
+    total_time = MPI_Wtime() - stime;
+#endif
+    if (my_rank == 0)
+        LOG_INFO("Total time to retrieve 1 tag from %11d objects: %7.2f , throughput %10.2f \n", n_query,
+                 total_time, n_query / total_time);
 
     free(values);
     free(obj_ids);
